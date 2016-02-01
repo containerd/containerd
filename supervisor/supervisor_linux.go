fb79ed86495b1996d4bb0f9281654e8006237017
Burcu Dogan <jbd@google.com>
2016-02-01 15:12:11 -0800

    libcontainer implementation of runtime must be in the runtime package
    Signed-off-by: Burcu Dogan <jbd@google.com>

--- supervisor/supervisor_linux.go
+++ supervisor/supervisor_linux.go
@@ -3,10 +3,9 @@
 package supervisor
 
 import (
-	"github.com/docker/containerd/linux"
 	"github.com/docker/containerd/runtime"
 )
 
 func newRuntime(stateDir string) (runtime.Runtime, error) {
-	return linux.NewRuntime(stateDir)
+	return runtime.NewRuntime(stateDir)
 }
