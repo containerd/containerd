aff808ece62d51632eca2368f8df940d81ae12a4
Kenfe-Mickael Laventure <mickael.laventure@gmail.com>
2017-01-23 15:27:12 -0800

    Update protobuf and grpc to match docker/swarmkit
    protobuf: 8ee79997227bf9b34611aee7946ae64735e6fd93
    grpc: v1.0.4
    
    Signed-off-by: Kenfe-Mickael Laventure <mickael.laventure@gmail.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -13,7 +13,7 @@ clone git github.com/docker/docker 2f6e3b0ba027b558adabd41344fee59db4441011
 clone git github.com/docker/go-units 5d2041e26a699eaca682e2ea41c8f891e1060444
 clone git github.com/godbus/dbus e2cf28118e66a6a63db46cf6088a35d2054d3bb0
 clone git github.com/golang/glog 23def4e6c14b4da8ac2ed8007337bc5eb5007998
-clone git github.com/golang/protobuf 1f49d83d9aa00e6ce4fc8258c71cc7786aec968a
+clone git github.com/golang/protobuf 8ee79997227bf9b34611aee7946ae64735e6fd93
 clone git github.com/opencontainers/runc 51371867a01c467f08af739783b8beafc154c4d7 https://github.com/docker/runc.git
 clone git github.com/opencontainers/runtime-spec 1c7c27d043c2a5e513a44084d2b10d77d1402b8c
 clone git github.com/rcrowley/go-metrics eeba7bd0dd01ace6e690fa833b3f22aaec29af43
@@ -23,7 +23,7 @@ clone git github.com/vishvananda/netlink adb0f53af689dd38f1443eba79489feaacf0b22
 clone git github.com/Azure/go-ansiterm 70b2c90b260171e829f1ebd7c17f600c11858dbe
 clone git golang.org/x/net 991d3e32f76f19ee6d9caadb3a22eae8d23315f7 https://github.com/golang/net.git
 clone git golang.org/x/sys d4feaf1a7e61e1d9e79e6c4e76c6349e9cab0a03 https://github.com/golang/sys.git
-clone git google.golang.org/grpc v1.0.1-GA https://github.com/grpc/grpc-go.git
+clone git google.golang.org/grpc v1.0.4 https://github.com/grpc/grpc-go.git
 clone git github.com/seccomp/libseccomp-golang 1b506fc7c24eec5a3693cdcbed40d9c226cfc6a1
 clone git github.com/tonistiigi/fifo b45391ebcd3d282404092c04a2b015b37df12383
 clone git github.com/pkg/errors 839d9e913e063e28dfd0e6c7b7512793e0a48be9
