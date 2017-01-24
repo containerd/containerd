ca523657807d83e24ee17f5bce2e4b76cbc56c23
Daniel Zhang <jmzwcn@gmail.com>
2017-01-24 18:16:16 +0800

    Allow specifying the log level
    Signed-off-by: Daniel Zhang <jmzwcn@gmail.com>

--- cmd/containerd/main.go
+++ cmd/containerd/main.go
@@ -49,6 +49,11 @@ high performance container runtime
 			Usage: "enable debug output in logs",
 		},
 		cli.StringFlag{
+			Name:  "log-level",
+			Usage: "Set the logging level [debug, info, warn, error, fatal, panic]",
+			Value: "info",
+		},
+		cli.StringFlag{
 			Name:  "root",
 			Usage: "containerd state directory",
 			Value: "/run/containerd",
@@ -77,6 +82,14 @@ high performance container runtime
 	app.Before = func(context *cli.Context) error {
 		if context.GlobalBool("debug") {
 			logrus.SetLevel(logrus.DebugLevel)
+		}
+		if logLevel := context.GlobalString("log-level"); logLevel != "" {
+			lvl, err := logrus.ParseLevel(logLevel)
+			if err != nil {
+				lvl = logrus.InfoLevel
+				fmt.Fprintf(os.Stderr, "Unable to parse logging level: %s\n, and being defaulted to info", logLevel)
+			}
+			logrus.SetLevel(lvl)
 		}
 		return nil
 	}
