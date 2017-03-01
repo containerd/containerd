186a9a2acd5e2722dd9e3c76d2d6b5db16c0b102
Ian Campbell <ian.campbell@docker.com>
2017-03-01 14:58:05 +0000

    Return DeleteResponse from ContainerService.Delete
    The message was defined but the method was returning empty, plumb through the
    result from the shim layer.
    
    Compile tested only.
    
    Signed-off-by: Ian Campbell <ian.campbell@docker.com>

--- runtime.go
+++ runtime.go
@@ -26,7 +26,7 @@ type Runtime interface {
 	// Containers returns all the current containers for the runtime
 	Containers() ([]Container, error)
 	// Delete removes the container in the runtime
-	Delete(context.Context, Container) error
+	Delete(context.Context, Container) (uint32, error)
 	// Events returns events for the runtime and all containers created by the runtime
 	Events(context.Context) <-chan *Event
 }
