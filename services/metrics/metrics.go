f9212348e4808020bd13ac8d7c15b8824cf6f53e
Michael Crosby <crosbymichael@gmail.com>
2017-03-14 16:16:49 -0700

    Add grpc prometheus metrics
    This provideds generic grpc metrics via prometheus
    
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>

---
+++ services/metrics/metrics.go
@@ -0,0 +1,26 @@
+package metrics
+
+import (
+	"github.com/docker/containerd/plugin"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"google.golang.org/grpc"
+)
+
+func init() {
+	plugin.Register("metrics-grpc", &plugin.Registration{
+		Type: plugin.GRPCPlugin,
+		Init: New,
+	})
+}
+
+func New(_ *plugin.InitContext) (interface{}, error) {
+	return &Service{}, nil
+}
+
+type Service struct {
+}
+
+func (s *Service) Register(server *grpc.Server) error {
+	grpc_prometheus.Register(server)
+	return nil
+}
