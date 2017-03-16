397499c2882b49428107fded770c420ddda96fb1
Derek McGowan <derek@mcgstyle.net>
2017-03-16 15:06:15 -0700

    Add root flag to boltdb tests
    Needed for CI to run root coverage
    
    Signed-off-by: Derek McGowan <derek@mcgstyle.net> (github: dmcgowan)

--- snapshot/storage/boltdb/bolt_test.go
+++ snapshot/storage/boltdb/bolt_test.go
@@ -7,6 +7,9 @@ import (
 
 	"github.com/docker/containerd/snapshot/storage"
 	"github.com/docker/containerd/snapshot/storage/testsuite"
+
+	// Does not require root but flag must be defined for snapshot tests
+	_ "github.com/docker/containerd/testutil"
 )
 
 func BenchmarkSuite(b *testing.B) {
