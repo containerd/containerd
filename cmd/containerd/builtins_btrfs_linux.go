55bf3c10f0f80d285f2385ff13d3ad86025353ab
Vincent Batts <vbatts@hashbangbash.com>
2017-05-04 17:30:23 -0400

    containerd: compile tags to exclude btrfs
    Leave in btrfs by default, but add go build tags to exclude it.
    `go build -tags containerd_no_btrfs` will leave that driver out.
    As the current containerd/btrfs code needs link to libbtrfs*.so, but not
    all distros provide it.
    
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

---
+++ cmd/containerd/builtins_btrfs_linux.go
@@ -0,0 +1,5 @@
+// +build !containerd_no_btrfs
+
+package main
+
+import _ "github.com/containerd/containerd/snapshot/btrfs"
--- cmd/containerd/builtins_linux.go
+++ cmd/containerd/builtins_linux.go
@@ -3,6 +3,5 @@ package main
 import (
 	_ "github.com/containerd/containerd/linux"
 	_ "github.com/containerd/containerd/metrics/cgroups"
-	_ "github.com/containerd/containerd/snapshot/btrfs"
 	_ "github.com/containerd/containerd/snapshot/overlay"
 )
