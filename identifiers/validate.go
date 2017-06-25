596b23b5af4391b4c4e939b71df7fa1de2bcad9a
xiekeyang <xiekeyang@huawei.com>
2017-06-25 22:11:15 +0800

    [WIP] Set up identifiers to ContainerIDs and TaskIDs
    fix issue #1061.
    
    This constrains ContainerIDs and TaskIDs following to Docker container
    name. And the max length is limited to 128 bytes.
    
    Signed-off-by: xiekeyang <xiekeyang@huawei.com>

--- identifiers/validate.go
+++ identifiers/validate.go
@@ -18,6 +18,8 @@ import (
 
 const (
 	label = `[A-Za-z][A-Za-z0-9]+(?:[-]+[A-Za-z0-9]+)*`
+
+	containerIdLable = `[a-zA-Z0-9][a-zA-Z0-9_.-]{0,127}`
 )
 
 var (
@@ -28,11 +30,19 @@ var (
 	identifierRe = regexp.MustCompile(reAnchor(label + reGroup("[.]"+reGroup(label)) + "*"))
 
 	errIdentifierInvalid = errors.Errorf("invalid, must match %v", identifierRe)
+
+	// containerIdRe validates that a identifier matches valid container and task id.
+	//
+	// TODO(xiekeyang): Current rules for container and task id, following up Docker
+	// container name.
+	containerIdRe = regexp.MustCompile(reAnchor(containerIdLable))
+
+	errContainerIdInvalid = errors.Errorf("invalid container and task id, must match %v", containerIdRe)
 )
 
 // IsInvalid return true if the error was due to an invalid identifer.
 func IsInvalid(err error) bool {
-	return errors.Cause(err) == errIdentifierInvalid
+	return errors.Cause(err) == errIdentifierInvalid || errors.Cause(err) == errContainerIdInvalid
 }
 
 // Validate return nil if the string s is a valid identifier.
@@ -45,6 +55,14 @@ func IsInvalid(err error) bool {
 func Validate(s string) error {
 	if !identifierRe.MatchString(s) {
 		return errors.Wrapf(errIdentifierInvalid, "identifier %q", s)
+	}
+	return nil
+}
+
+// ValidateContainerId return nil if the string s is a valid container and task ID.
+func ValidateContainerId(s string) error {
+	if !containerIdRe.MatchString(s) {
+		return errors.Wrapf(errContainerIdInvalid, "identifier %q", s)
 	}
 	return nil
 }
