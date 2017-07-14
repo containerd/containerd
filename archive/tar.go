4a3f98cb6175c1244c857c5d00905e69d4db1b20
Derek McGowan <derek@mcgstyle.net>
2017-07-14 14:09:22 -0700

    Add link breakout checks and tests
    Ensure symlinks cannot be used to breakout of unpack directory.
    Evaluate absolute symlinks as scoped to unpack directory.
    Allow symlinks which point outside the root to be created.
    Scope all resolution of symlinks to the unpack directory.
    
    Signed-off-by: Derek McGowan <derek@mcgstyle.net>

--- archive/tar.go
+++ archive/tar.go
@@ -25,8 +25,6 @@ var (
 			return make([]byte, 32*1024)
 		},
 	}
-
-	breakoutError = errors.New("file name outside of root")
 )
 
 // Diff returns a tar stream of the computed filesystem
@@ -168,16 +166,11 @@ func Apply(ctx context.Context, root string, r io.Reader) (int64, error) {
 			}
 		}
 
-		path := filepath.Join(root, hdr.Name)
-		rel, err := filepath.Rel(root, path)
+		path, err := rootPath(root, hdr.Name)
 		if err != nil {
-			return 0, err
+			return 0, errors.Wrap(err, "failed to get root path")
 		}
 
-		// Note as these operations are platform specific, so must the slash be.
-		if strings.HasPrefix(rel, ".."+string(os.PathSeparator)) {
-			return 0, errors.Wrapf(breakoutError, "%q is outside of %q", hdr.Name, root)
-		}
 		base := filepath.Base(path)
 
 		if strings.HasPrefix(base, whiteoutPrefix) {
@@ -467,25 +460,15 @@ func createTarFile(ctx context.Context, path, extractDir string, hdr *tar.Header
 		}
 
 	case tar.TypeLink:
-		targetPath := filepath.Join(extractDir, hdr.Linkname)
-		// check for hardlink breakout
-		if !strings.HasPrefix(targetPath, extractDir) {
-			return errors.Wrapf(breakoutError, "invalid hardlink %q -> %q", targetPath, hdr.Linkname)
+		targetPath, err := rootPath(extractDir, hdr.Linkname)
+		if err != nil {
+			return err
 		}
 		if err := os.Link(targetPath, path); err != nil {
 			return err
 		}
 
 	case tar.TypeSymlink:
-		// 	path 				-> hdr.Linkname = targetPath
-		// e.g. /extractDir/path/to/symlink 	-> ../2/file	= /extractDir/path/2/file
-		targetPath := filepath.Join(filepath.Dir(path), hdr.Linkname)
-
-		// the reason we don't need to check symlinks in the path (with FollowSymlinkInScope) is because
-		// that symlink would first have to be created, which would be caught earlier, at this very check:
-		if !strings.HasPrefix(targetPath, extractDir) {
-			return errors.Wrapf(breakoutError, "invalid symlink %q -> %q", path, hdr.Linkname)
-		}
 		if err := os.Symlink(hdr.Linkname, path); err != nil {
 			return err
 		}
