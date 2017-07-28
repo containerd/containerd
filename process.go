70da86dc5c4758843148f153f240a05656be711f
Michael Crosby <crosbymichael@gmail.com>
2017-07-28 10:40:22 -0400

    Return err on Wait when Task/Process exited
    Fixes #1054
    
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>

--- process.go
+++ process.go
@@ -2,17 +2,31 @@ package containerd
 
 import (
 	"context"
+	"sync"
 	"syscall"
 
 	eventsapi "github.com/containerd/containerd/api/services/events/v1"
 	"github.com/containerd/containerd/api/services/tasks/v1"
+	"github.com/containerd/containerd/errdefs"
 	"github.com/containerd/containerd/typeurl"
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 )
 
+type Process interface {
+	Pid() uint32
+	Start(context.Context) error
+	Delete(context.Context) (uint32, error)
+	Kill(context.Context, syscall.Signal) error
+	Wait(context.Context) (uint32, error)
+	CloseIO(context.Context, ...IOCloserOpts) error
+	Resize(ctx context.Context, w, h uint32) error
+	IO() *IO
+}
+
 type process struct {
 	id   string
 	task *task
+	mu   sync.Mutex
 	pid  uint32
 	io   *IO
 	spec *specs.Process
@@ -48,25 +62,42 @@ func (p *process) Start(ctx context.Context) error {
 		p.io.Cancel()
 		p.io.Wait()
 		p.io.Close()
-		return err
+		return errdefs.FromGRPC(err)
 	}
+	p.mu.Lock()
 	p.pid = response.Pid
+	p.mu.Unlock()
 	return nil
 }
 
+// Kill sends the provided Signal to the process
 func (p *process) Kill(ctx context.Context, s syscall.Signal) error {
-	_, err := p.task.client.TaskService().Kill(ctx, &tasks.KillRequest{
+	if _, err := p.task.client.TaskService().Kill(ctx, &tasks.KillRequest{
 		Signal:      uint32(s),
 		ContainerID: p.task.id,
 		ExecID:      p.id,
-	})
-	return err
+	}); err != nil {
+		return errdefs.FromGRPC(err)
+	}
+	return nil
 }
 
+// Wait is a blocking call that will wait for the process to exit and return the exit status
+//
+// Wait returns an ErrUnavailable when the task has already run and stopped
 func (p *process) Wait(ctx context.Context) (uint32, error) {
 	eventstream, err := p.task.client.EventService().Subscribe(ctx, &eventsapi.SubscribeRequest{})
 	if err != nil {
 		return UnknownExitStatus, err
+	}
+	// first check if the process has exited only if we have a pid
+	p.mu.Lock()
+	started := p.pid != 0
+	p.mu.Unlock()
+	if started {
+		if err := p.Kill(ctx, 0); err != nil {
+			return UnknownExitStatus, errdefs.ErrUnavailable
+		}
 	}
 	for {
 		evt, err := eventstream.Recv()
@@ -96,8 +127,10 @@ func (p *process) CloseIO(ctx context.Context, opts ...IOCloserOpts) error {
 		o(&i)
 	}
 	r.Stdin = i.Stdin
-	_, err := p.task.client.TaskService().CloseIO(ctx, r)
-	return err
+	if _, err := p.task.client.TaskService().CloseIO(ctx, r); err != nil {
+		return errdefs.FromGRPC(err)
+	}
+	return nil
 }
 
 func (p *process) IO() *IO {
@@ -105,13 +138,15 @@ func (p *process) IO() *IO {
 }
 
 func (p *process) Resize(ctx context.Context, w, h uint32) error {
-	_, err := p.task.client.TaskService().ResizePty(ctx, &tasks.ResizePtyRequest{
+	if _, err := p.task.client.TaskService().ResizePty(ctx, &tasks.ResizePtyRequest{
 		ContainerID: p.task.id,
 		Width:       w,
 		Height:      h,
 		ExecID:      p.id,
-	})
-	return err
+	}); err != nil {
+		return errdefs.FromGRPC(err)
+	}
+	return nil
 }
 
 func (p *process) Delete(ctx context.Context) (uint32, error) {
@@ -124,7 +159,7 @@ func (p *process) Delete(ctx context.Context) (uint32, error) {
 		ExecID:      p.id,
 	})
 	if err != nil {
-		return UnknownExitStatus, err
+		return UnknownExitStatus, errdefs.FromGRPC(err)
 	}
 	return r.ExitStatus, nil
 }
