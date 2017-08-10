661bcd3e4d95e6d55f42b3bedbc18c6f8b8e5793
Christopher Jones <tophj@linux.vnet.ibm.com>
2017-08-10 13:07:28 -0400

    Change test image based on platform
    This changes testImage so it can be run on other platforms.
    
    Signed-off-by: Christopher Jones <tophj@linux.vnet.ibm.com>

--- client_unix_test.go
+++ client_unix_test.go
@@ -2,12 +2,36 @@
 
 package containerd
 
+import (
+	"runtime"
+)
+
 const (
 	defaultRoot    = "/var/lib/containerd-test"
 	defaultAddress = "/run/containerd-test/containerd.sock"
-	testImage      = "docker.io/library/alpine:latest"
+)
+
+var (
+	testImage string
 )
 
 func platformTestSetup(client *Client) error {
 	return nil
+}
+
+func init() {
+	switch runtime.GOARCH {
+	case "386":
+		testImage = "docker.io/i386/alpine:latest"
+	case "arm":
+		testImage = "docker.io/arm32v6/alpine:latest"
+	case "arm64":
+		testImage = "docker.io/arm64v8/alpine:latest"
+	case "ppc64le":
+		testImage = "docker.io/ppc64le/alpine:latest"
+	case "s390x":
+		testImage = "docker.io/s390x/alpine:latest"
+	default:
+		testImage = "docker.io/library/alpine:latest"
+	}
 }
