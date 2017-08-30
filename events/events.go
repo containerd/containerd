9255e752b3013d2d7a506cf6b019181334b9cf5e
Stephen J Day <stephen.day@docker.com>
2017-08-30 15:22:35 -0700

    containerd: export Subscribe method on client
    Signed-off-by: Stephen J Day <stephen.day@docker.com>

--- events/events.go
+++ events/events.go
@@ -22,3 +22,7 @@ type publisherFunc func(ctx context.Context, topic string, event Event) error
 func (fn publisherFunc) Publish(ctx context.Context, topic string, event Event) error {
 	return fn(ctx, topic, event)
 }
+
+type Subscriber interface {
+	Subscribe(ctx context.Context, filters ...string) (ch <-chan *events.Envelope, errs <-chan error)
+}
