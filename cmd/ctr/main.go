0895dbe93244ede91b975f8a16db1eb63d4b54f2
Jess Valarezo <valarezo.jessica@gmail.com>
2017-10-25 13:22:53 -0700

    ctr: move content command
    Signed-off-by: Jess Valarezo <valarezo.jessica@gmail.com>

--- cmd/ctr/main.go
+++ cmd/ctr/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/containerd/containerd/cmd/ctr/commands/containers"
+	"github.com/containerd/containerd/cmd/ctr/commands/content"
 	"github.com/containerd/containerd/cmd/ctr/commands/images"
 	"github.com/containerd/containerd/cmd/ctr/commands/plugins"
 	versionCmd "github.com/containerd/containerd/cmd/ctr/commands/version"
@@ -72,7 +73,7 @@ containerd CLI
 		versionCmd.Command,
 		applyCommand,
 		containers.Command,
-		contentCommand,
+		content.Command,
 		eventsCommand,
 		fetchCommand,
 		fetchObjectCommand,
