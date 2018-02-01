ea00fc793bd1dca2e944bcd1fc8b576fe995c753
Michael Crosby <crosbymichael@gmail.com>
2018-02-01 17:18:40 -0500

    Remove protobuf for mount in shim
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>

--- linux/proc/types.go
+++ linux/proc/types.go
@@ -1,16 +1,22 @@
 package proc
 
 import (
-	containerd_types "github.com/containerd/containerd/api/types"
 	google_protobuf "github.com/gogo/protobuf/types"
 )
+
+type Mount struct {
+	Type    string
+	Source  string
+	Target  string
+	Options []string
+}
 
 // CreateConfig hold task creation configuration
 type CreateConfig struct {
 	ID               string
 	Bundle           string
 	Runtime          string
-	Rootfs           []*containerd_types.Mount
+	Rootfs           []Mount
 	Terminal         bool
 	Stdin            string
 	Stdout           string
