f4113a903a4de43a0f0e89b64ac76546240d2c7f
Stephen J Day <stephen.day@docker.com>
2018-02-02 17:05:53 -0800

    containerd: include zfs plugin by default
    Signed-off-by: Stephen J Day <stephen.day@docker.com>

--- cmd/containerd/builtins_linux.go
+++ cmd/containerd/builtins_linux.go
@@ -20,4 +20,5 @@ import (
 	_ "github.com/containerd/containerd/linux"
 	_ "github.com/containerd/containerd/metrics/cgroups"
 	_ "github.com/containerd/containerd/snapshots/overlay"
+	_ "github.com/containerd/zfs"
 )
