09aff83bdd01ad628262d180cdbaf6b4bf5c5f7e
Lantao Liu <lantaol@google.com>
2018-02-09 08:53:26 +0000

    Do not fast build.
    Signed-off-by: Lantao Liu <lantaol@google.com>

--- hack/install-deps.sh
+++ hack/install-deps.sh
@@ -105,7 +105,7 @@ ${sudo} make install -e DESTDIR=${RUNC_DIR}
 if ${INSTALL_CNI}; then
   checkout_repo ${CNI_PKG} ${CNI_VERSION} ${CNI_REPO}
   cd ${GOPATH}/src/${CNI_PKG}
-  FASTBUILD=true ./build.sh
+  ./build.sh
   ${sudo} mkdir -p ${CNI_DIR}
   ${sudo} cp -r ./bin ${CNI_DIR}
   ${sudo} mkdir -p ${CNI_CONFIG_DIR}
