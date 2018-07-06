8b42adeddcf6a5fe9114d45e34bf11d19f8e879f
Sebastiaan van Stijn <github@gone.nl>
2018-07-06 18:34:08 +0200

    Add /proc/acpi to masked paths
    relates to CVE-2018-10892
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- oci/spec_unix.go
+++ oci/spec_unix.go
@@ -153,6 +153,7 @@ func createDefaultSpec(ctx context.Context, id string) (*Spec, error) {
 		},
 		Linux: &specs.Linux{
 			MaskedPaths: []string{
+				"/proc/acpi",
 				"/proc/kcore",
 				"/proc/latency_stats",
 				"/proc/timer_list",
