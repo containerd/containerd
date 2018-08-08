4d629f32534063b1f974e36cb4b4d9cf8e41571d
Ian Campbell <ian.campbell@docker.com>
2018-08-08 10:09:05 +0100

    oci: Update docs for `oci.WithUserID`
    The behaviour was changed in 99df1a9e11f3 ("Set gid 0 when no group is
    specified"), part of #2529.
    
    Take the opportunity to tighten up the grammar a bit too.
    
    Signed-off-by: Ian Campbell <ian.campbell@docker.com>
    Signed-off-by: Phil Estes <estesp@linux.vnet.ibm.com>

--- oci/spec_opts_unix.go
+++ oci/spec_opts_unix.go
@@ -348,8 +348,8 @@ func WithUIDGID(uid, gid uint32) SpecOpts {
 
 // WithUserID sets the correct UID and GID for the container based
 // on the image's /etc/passwd contents. If /etc/passwd does not exist,
-// or uid is not found in /etc/passwd, it sets gid to be the same with
-// uid, and not returns error.
+// or uid is not found in /etc/passwd, it sets the requested uid,
+// additionally sets the gid to 0, and does not return an error.
 func WithUserID(uid uint32) SpecOpts {
 	return func(ctx context.Context, client Client, c *containers.Container, s *specs.Spec) (err error) {
 		setProcess(s)
