55842b1713a9d19fec6f1eb2853af3eaad2054fd
Michael Wan <zirenwan@gmail.com>
2018-08-29 04:58:44 -0400

    vendor: update github.com/containerd/continuity commit to f44b615
    Signed-off-by: Michael Wan <zirenwan@gmail.com>

--- vendor/github.com/containerd/continuity/ioutils.go
+++ vendor/github.com/containerd/continuity/ioutils.go
@@ -1,26 +1,34 @@
 package continuity
 
 import (
+	"bytes"
 	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 )
 
+// AtomicWriteFile atomically writes data to a file by first writing to a
+// temp file and calling rename.
+func AtomicWriteFile(filename string, data []byte, perm os.FileMode) error {
+	buf := bytes.NewBuffer(data)
+	return atomicWriteFile(filename, buf, int64(len(data)), perm)
+}
+
 // atomicWriteFile writes data to a file by first writing to a temp
 // file and calling rename.
-func atomicWriteFile(filename string, r io.Reader, rf RegularFile) error {
+func atomicWriteFile(filename string, r io.Reader, dataSize int64, perm os.FileMode) error {
 	f, err := ioutil.TempFile(filepath.Dir(filename), ".tmp-"+filepath.Base(filename))
 	if err != nil {
 		return err
 	}
-	err = os.Chmod(f.Name(), rf.Mode())
+	err = os.Chmod(f.Name(), perm)
 	if err != nil {
 		f.Close()
 		return err
 	}
 	n, err := io.Copy(f, r)
-	if err == nil && n < rf.Size() {
+	if err == nil && n < dataSize {
 		f.Close()
 		return io.ErrShortWrite
 	}
