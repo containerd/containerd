51ad4d302012ae442db8fc38f669f222103a9f76
Lifubang <lifubang@acmcoder.com>
2018-11-16 14:09:39 +0800

    change args usage for ctr c create
    Signed-off-by: Lifubang <lifubang@acmcoder.com>

--- cmd/ctr/commands/containers/containers.go
+++ cmd/ctr/commands/containers/containers.go
@@ -51,7 +51,7 @@ var Command = cli.Command{
 var createCommand = cli.Command{
 	Name:      "create",
 	Usage:     "create container",
-	ArgsUsage: "[flags] Image|RootFS CONTAINER",
+	ArgsUsage: "[flags] Image|RootFS CONTAINER [COMMAND] [ARG...]",
 	Flags:     append(commands.SnapshotterFlags, commands.ContainerFlags...),
 	Action: func(context *cli.Context) error {
 		var (
