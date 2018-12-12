2db0c6c2622e59ac20833eba266a2229d8580e7e
Eric Lin <linxiulei@gmail.com>
2018-12-12 12:01:35 +0800

    Explicitly stating utf-8 when fetching oauth token
    Fixes #2870
    
    Signed-off-by: Eric Lin <linxiulei@gmail.com>

--- remotes/docker/authorizer.go
+++ remotes/docker/authorizer.go
@@ -194,7 +194,11 @@ func (a *dockerAuthorizer) fetchTokenWithOAuth(ctx context.Context, to tokenOpti
 		form.Set("password", to.secret)
 	}
 
-	resp, err := ctxhttp.PostForm(ctx, a.client, to.realm, form)
+	resp, err := ctxhttp.Post(
+		ctx, a.client, to.realm,
+		"application/x-www-form-urlencoded; charset=utf-8",
+		strings.NewReader(form.Encode()),
+	)
 	if err != nil {
 		return "", err
 	}
