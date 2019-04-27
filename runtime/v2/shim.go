79f6941d219f7a74cc05d9473628821889808284
Sebastiaan van Stijn <github@gone.nl>
2019-04-27 15:30:18 -0700

    bump containerd/ttrpc 699c4e40d1e7416e08bf7019c7ce2e9beced4636
    full diff: https://github.com/containerd/ttrpc/compare/f02858b1457c5ca3aaec3a0803eb0d59f96e41d6...699c4e40d1e7416e08bf7019c7ce2e9beced4636
    
    - containerd/ttrpc#33 Fix returns error message
    - containerd/ttrpc#35 Make onclose an option
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 8c5779c32b70a0c55e1c94eb45b305897f7cf3f1)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- runtime/v2/shim.go
+++ runtime/v2/shim.go
@@ -75,8 +75,7 @@ func loadShim(ctx context.Context, bundle *Bundle, events *exchange.Exchange, rt
 		}
 	}()
 
-	client := ttrpc.NewClient(conn)
-	client.OnClose(func() { conn.Close() })
+	client := ttrpc.NewClient(conn, ttrpc.WithOnClose(func() { _ = conn.Close() }))
 	s := &shim{
 		client:  client,
 		task:    task.NewTaskClient(client),
