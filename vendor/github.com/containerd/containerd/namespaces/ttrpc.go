7f330dc4aa4bd6b2b051f45209c480d0899a5e42
Lantao Liu <lantaol@google.com>
2019-08-10 10:40:43 -0700

    Update containerd to fix panic caused by race condition.
    Signed-off-by: Lantao Liu <lantaol@google.com>

--- vendor/github.com/containerd/containerd/namespaces/ttrpc.go
+++ vendor/github.com/containerd/containerd/namespaces/ttrpc.go
@@ -27,10 +27,20 @@ const (
 	TTRPCHeader = "containerd-namespace-ttrpc"
 )
 
+func copyMetadata(src ttrpc.MD) ttrpc.MD {
+	md := ttrpc.MD{}
+	for k, v := range src {
+		md[k] = append(md[k], v...)
+	}
+	return md
+}
+
 func withTTRPCNamespaceHeader(ctx context.Context, namespace string) context.Context {
 	md, ok := ttrpc.GetMetadata(ctx)
 	if !ok {
 		md = ttrpc.MD{}
+	} else {
+		md = copyMetadata(md)
 	}
 	md.Set(TTRPCHeader, namespace)
 	return ttrpc.WithMetadata(ctx, md)
