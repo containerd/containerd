56ea6aaaab98b7e1cfa972f07e030cd66cb85b40
Michael Crosby <crosbymichael@gmail.com>
2019-09-06 16:25:02 -0400

    Add --env-file to ctr
    Closes #3517
    
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>

--- cmd/ctr/commands/run/run_unix.go
+++ cmd/ctr/commands/run/run_unix.go
@@ -64,6 +64,9 @@ func NewContainer(ctx gocontext.Context, client *containerd.Client, context *cli
 			args = context.Args()[2:]
 		)
 		opts = append(opts, oci.WithDefaultSpec(), oci.WithDefaultUnixDevices)
+		if ef := context.String("env-file"); ef != "" {
+			opts = append(opts, oci.WithEnvFile(ef))
+		}
 		opts = append(opts, oci.WithEnv(context.StringSlice("env")))
 		opts = append(opts, withMounts(context))
 
