03cb006cddaf9285113d4372125fbba15f472c0a
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2020-02-19 01:46:16 +0900

    support building with BUILDTAGS="no_zfs no_aufs"
    These plugins do not always need to be built-in.
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- cmd/containerd/builtins_aufs_linux.go
+++ cmd/containerd/builtins_aufs_linux.go
@@ -1,3 +1,5 @@
+// +build !no_aufs
+
 /*
    Copyright The containerd Authors.
 
@@ -16,12 +18,4 @@
 
 package main
 
-import (
-	_ "github.com/containerd/containerd/metrics/cgroups"
-	_ "github.com/containerd/containerd/metrics/cgroups/v2"
-	_ "github.com/containerd/containerd/runtime/v1/linux"
-	_ "github.com/containerd/containerd/runtime/v2"
-	_ "github.com/containerd/containerd/runtime/v2/runc/options"
-	_ "github.com/containerd/containerd/snapshots/native"
-	_ "github.com/containerd/containerd/snapshots/overlay"
-)
+import _ "github.com/containerd/aufs"
