c5078a5b7248c386557f89239fef27c19cc39b06
Sebastiaan van Stijn <github@gone.nl>
2020-05-11 23:02:40 +0200

    vendor: containerd/continuity d3ef23f19fbb106bb73ffde425d07a9187e30745
    full diff: https://github.com/containerd/continuity/compare/0ec596719c75bfd42908850990acea594b7593ac...d3ef23f19fbb106bb73ffde425d07a9187e30745
    
    - fs: support for OpenBSD
    - sysx/xattr: fix and improve
    - remove Windows' Readlink fork
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/github.com/containerd/continuity/driver/driver_windows.go
+++ vendor/github.com/containerd/continuity/driver/driver_windows.go
@@ -14,12 +14,16 @@
    limitations under the License.
 */
 
+// +build go1.13
+
+// Go 1.13 is the minimally supported version for Windows.
+// Earlier golang releases have bug in os.Readlink
+// (see https://github.com/golang/go/issues/30463).
+
 package driver
 
 import (
 	"os"
-
-	"github.com/containerd/continuity/sysx"
 )
 
 func (d *driver) Mknod(path string, mode os.FileMode, major, minor int) error {
@@ -34,10 +38,4 @@ func (d *driver) Mkfifo(path string, mode os.FileMode) error {
 func (d *driver) Lchmod(path string, mode os.FileMode) (err error) {
 	// TODO: Use Window's equivalent
 	return os.Chmod(path, mode)
-}
-
-// Readlink is forked in order to support Volume paths which are used
-// in container layers.
-func (d *driver) Readlink(p string) (string, error) {
-	return sysx.Readlink(p)
 }
