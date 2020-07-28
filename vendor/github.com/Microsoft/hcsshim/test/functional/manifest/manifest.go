474213b03cece29c6b11b66aec5dd159ff260187
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2020-07-29 03:24:46 +1000

    Import Windows manifest into test build, rather than copying by script
    This will ensure that we can always get the current Windows OS build
    version, without being put into Windows 8 compatibility mode.
    
    Signed-off-by: Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>

---
+++ vendor/github.com/Microsoft/hcsshim/test/functional/manifest/manifest.go
@@ -0,0 +1,4 @@
+package manifest
+
+// This is so that tests can include the .syso to manifest them to pick up the right Windows build
+// TODO: Auto-generation of the .syso through rsrc or similar.
