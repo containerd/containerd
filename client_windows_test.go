1ec1e9eabfbf3290e2a69b9a787a24c318535c14
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2020-07-28 22:36:00 +1000

    Add Windows 10/Windows Server 2004 for integration tests
    Signed-off-by: Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>

--- client_windows_test.go
+++ client_windows_test.go
@@ -51,6 +51,8 @@ func init() {
 		testImage = "mcr.microsoft.com/windows/nanoserver:1903"
 	case 18363: // this isn't in osversion yet, but the image should be available
 		testImage = "mcr.microsoft.com/windows/nanoserver:1909"
+	case 19041: // this isn't in osversion yet, but the image should be available
+		testImage = "mcr.microsoft.com/windows/nanoserver:2004"
 	case 9200: // Missing manifest, so it's running in compatibility mode
 		fmt.Println("You need to copy Microsoft/hcsshim/test/functional/manifest/rsrc_amd64.syso into the containerd checkout")
 		panic("Running in Windows 8/Windows Server 2012 compatibility mode, failed to detect Windows build version")
