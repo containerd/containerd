1f4192daf48150b2552a47c7a00f733bd4fdadad
Samuel Karp <me@samuelkarp.com>
2020-12-11 22:21:26 -0800

    freebsd: exclude v1 runtimes
    Signed-off-by: Samuel Karp <me@samuelkarp.com>

--- services/tasks/local_freebsd.go
+++ services/tasks/local_freebsd.go
@@ -1,4 +1,4 @@
-// +build !windows,!freebsd
+// +build freebsd
 
 /*
    Copyright The containerd Authors.
@@ -19,38 +19,17 @@
 package tasks
 
 import (
-	"github.com/containerd/containerd/log"
 	"github.com/containerd/containerd/plugin"
 	"github.com/containerd/containerd/runtime"
-	"github.com/pkg/errors"
 )
 
 var tasksServiceRequires = []plugin.Type{
-	plugin.RuntimePlugin,
 	plugin.RuntimePluginV2,
 	plugin.MetadataPlugin,
 	plugin.TaskMonitorPlugin,
 }
 
+// loadV1Runtimes on FreeBSD returns an empty map. There are no v1 runtimes
 func loadV1Runtimes(ic *plugin.InitContext) (map[string]runtime.PlatformRuntime, error) {
-	rt, err := ic.GetByType(plugin.RuntimePlugin)
-	if err != nil {
-		return nil, err
-	}
-
-	runtimes := make(map[string]runtime.PlatformRuntime)
-	for _, rr := range rt {
-		ri, err := rr.Instance()
-		if err != nil {
-			log.G(ic.Context).WithError(err).Warn("could not load runtime instance due to initialization error")
-			continue
-		}
-		r := ri.(runtime.PlatformRuntime)
-		runtimes[r.ID()] = r
-	}
-
-	if len(runtimes) == 0 {
-		return nil, errors.New("no runtimes available to create task service")
-	}
-	return runtimes, nil
+	return make(map[string]runtime.PlatformRuntime), nil
 }
