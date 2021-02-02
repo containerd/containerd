05a2e280ac05606bb1782fb2c171e7afef74a8a6
Kazuyoshi Kato <katokazu@amazon.com>
2021-02-02 14:57:34 -0800

    mount: make setupLoop() work with with Autoclear
    setupLoop()'s Autoclear (LO_FLAGS_AUTOCLEAR) will destruct the
    loopback device when all associated file descriptors are closed.
    
    However this behavior didn't work before since setupLoop() was
    returning a file name. The looppack device was destructed at
    the end of the function when LoopParams had Autoclear = true.
    
    Fixes #4969.
    
    Signed-off-by: Kazuyoshi Kato <katokazu@amazon.com>

--- mount/losetup_linux_test.go
+++ mount/losetup_linux_test.go
@@ -64,12 +64,13 @@ func TestRoLoop(t *testing.T) {
 		}
 	}()
 
-	path, err := setupLoop(backingFile, LoopParams{Readonly: true, Autoclear: true})
+	file, err := setupLoop(backingFile, LoopParams{Readonly: true, Autoclear: true})
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer file.Close()
 
-	if err := ioutil.WriteFile(path, randomData, os.ModePerm); err == nil {
+	if _, err := file.Write(randomData); err == nil {
 		t.Fatalf("writing to readonly loop device should fail")
 	}
 }
@@ -84,12 +85,13 @@ func TestRwLoop(t *testing.T) {
 		}
 	}()
 
-	path, err := setupLoop(backingFile, LoopParams{Autoclear: true})
+	file, err := setupLoop(backingFile, LoopParams{Autoclear: false})
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer file.Close()
 
-	if err := ioutil.WriteFile(path, randomData, os.ModePerm); err != nil {
+	if _, err := file.Write(randomData); err != nil {
 		t.Fatal(err)
 	}
 }
