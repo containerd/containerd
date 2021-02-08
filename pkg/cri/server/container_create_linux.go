08318b1ab952e8c30908a9bd2855466d5bbe0ad9
zhangyadong.0808 <zhangyadong.0808@bytedance.com>
2021-02-09 06:12:47 +0800

    cri: append envs from image config to empty slice to avoid env lost
    Signed-off-by: Yadong Zhang <yadzhang@gmail.com>

--- pkg/cri/server/container_create_linux.go
+++ pkg/cri/server/container_create_linux.go
@@ -161,7 +161,7 @@ func (c *criService) containerSpec(
 
 	// Apply envs from image config first, so that envs from container config
 	// can override them.
-	env := imageConfig.Env
+	env := append([]string{}, imageConfig.Env...)
 	for _, e := range config.GetEnvs() {
 		env = append(env, e.GetKey()+"="+e.GetValue())
 	}
