19e96b3402e48218e4e4dcf8d892313c4213f9b7
Gijs Peskens <gijs@peskens.net>
2021-05-10 15:21:11 +0000

    Allow Linux on FreeBSD
    Signed-off-by: Gijs Peskens <gijs@peskens.net>

--- platforms/defaults_freebsd.go
+++ platforms/defaults_freebsd.go
@@ -1,5 +1,3 @@
-// +build !windows,!freebsd
-
 /*
    Copyright The containerd Authors.
 
@@ -18,7 +16,16 @@
 
 package platforms
 
+
+import (
+        "runtime"
+        specs "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
 // Default returns the default matcher for the platform.
 func Default() MatchComparer {
-	return Only(DefaultSpec())
+	return Ordered(DefaultSpec(), specs.Platform{
+		OS:		"linux",
+		Architecture:	runtime.GOARCH,
+	})
 }
