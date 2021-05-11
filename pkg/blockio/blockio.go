bebce477b750dd587b39a9788cda42bf4732fd68
Antti Kervinen <antti.kervinen@intel.com>
2021-05-11 09:45:09 +0300

    blockio: add blockio plugin
    Signed-off-by: Antti Kervinen <antti.kervinen@intel.com>

---
+++ pkg/blockio/blockio.go
@@ -0,0 +1,58 @@
+// +build linux
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package blockio
+
+import (
+	"github.com/containerd/containerd/log"
+	"github.com/intel/goresctrl/pkg/blockio"
+	"github.com/pkg/errors"
+
+	"github.com/containerd/containerd/plugin"
+)
+
+// Config contains the configuration of the blockio plugin
+type Config struct {
+	ConfigFile string `toml:"config_file" json:"configFile"`
+}
+
+func init() {
+	plugin.Register(&plugin.Registration{
+		Type:   plugin.InternalPlugin,
+		ID:     "blockio",
+		Config: &Config{},
+		InitFn: initBlockio,
+	})
+}
+
+func initBlockio(ic *plugin.InitContext) (interface{}, error) {
+	config, ok := ic.Config.(*Config)
+	if !ok {
+		return nil, errors.New("invalid blockio configuration")
+	}
+
+	if config.ConfigFile == "" {
+		log.L.Info("No blockio config file specified, blockio not configured")
+		return nil, nil
+	}
+
+	if err := blockio.SetConfigFromFile(config.ConfigFile, true); err != nil {
+		return nil, errors.Wrap(err, "configuring blockio failed")
+	}
+
+	return nil, nil
+}
