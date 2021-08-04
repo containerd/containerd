7917da764c6d6b0851f3adf1ef2346d62c88a1d0
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2021-08-04 14:12:32 -0700

    Change default directories on Darwin
    Signed-off-by: Maksym Pavlenko <pavlenko.maksym@gmail.com>

--- defaults/defaults_darwin.go
+++ defaults/defaults_darwin.go
@@ -1,4 +1,4 @@
-// +build !windows,!darwin
+// +build darwin
 
 /*
    Copyright The containerd Authors.
@@ -24,16 +24,17 @@ const (
 	DefaultRootDir = "/var/lib/containerd"
 	// DefaultStateDir is the default location used by containerd to store
 	// transient data
-	DefaultStateDir = "/run/containerd"
+	DefaultStateDir = "/var/run/containerd"
 	// DefaultAddress is the default unix socket address
-	DefaultAddress = "/run/containerd/containerd.sock"
+	DefaultAddress = "/var/run/containerd/containerd.sock"
 	// DefaultDebugAddress is the default unix socket address for pprof data
-	DefaultDebugAddress = "/run/containerd/debug.sock"
+	DefaultDebugAddress = "/var/run/containerd/debug.sock"
 	// DefaultFIFODir is the default location used by client-side cio library
 	// to store FIFOs.
-	DefaultFIFODir = "/run/containerd/fifo"
-	// DefaultRuntime is the default linux runtime
-	DefaultRuntime = "io.containerd.runc.v2"
+	DefaultFIFODir = "/var/run/containerd/fifo"
+	// DefaultRuntime is the default Darwin runtime.
+	// NOTE: there is no runtime on Darwin as of now.
+	DefaultRuntime = ""
 	// DefaultConfigDir is the default location for config files.
 	DefaultConfigDir = "/etc/containerd"
 )
