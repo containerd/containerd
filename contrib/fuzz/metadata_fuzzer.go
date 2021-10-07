3a713811bee3bd813853a0c87763ddf2d804adee
zounengren <zounengren@cmss.chinamobile.com>
2021-10-07 11:27:42 +0800

    run `gofmt` with Go 1.17
    Signed-off-by: Zou Nengren <zouyee1989@gmail.com>

--- contrib/fuzz/metadata_fuzzer.go
+++ contrib/fuzz/metadata_fuzzer.go
@@ -1,3 +1,4 @@
+//go:build gofuzz
 // +build gofuzz
 
 /*
@@ -12,7 +13,6 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-
 
 package fuzz
 
