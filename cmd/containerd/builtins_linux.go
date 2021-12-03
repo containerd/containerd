d2f1dda0f1e6eef141538ff691e9db43a75cdad0
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2021-12-03 11:30:03 -0800

    Include runtime v2 in default builtins
    Signed-off-by: Maksym Pavlenko <pavlenko.maksym@gmail.com>

--- cmd/containerd/builtins_linux.go
+++ cmd/containerd/builtins_linux.go
@@ -20,7 +20,6 @@ import (
 	_ "github.com/containerd/containerd/metrics/cgroups"
 	_ "github.com/containerd/containerd/metrics/cgroups/v2"
 	_ "github.com/containerd/containerd/runtime/v1/linux"
-	_ "github.com/containerd/containerd/runtime/v2"
 	_ "github.com/containerd/containerd/runtime/v2/runc/options"
 	_ "github.com/containerd/containerd/snapshots/native/plugin"
 	_ "github.com/containerd/containerd/snapshots/overlay/plugin"
