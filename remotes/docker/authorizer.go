c0e00f19abe3aabe04769b75fea51b7b5cb69ee7
ye.sijun <junnplus@gmail.com>
2022-01-27 17:34:27 +0800

    fix acr fetch token 400
    Signed-off-by: ye.sijun <junnplus@gmail.com>

--- remotes/docker/authorizer.go
+++ remotes/docker/authorizer.go
@@ -311,7 +311,8 @@ func (ah *authHandler) doBearerAuth(ctx context.Context) (token, refreshToken st
 				// Registries without support for POST may return 404 for POST /v2/token.
 				// As of September 2017, GCR is known to return 404.
 				// As of February 2018, JFrog Artifactory is known to return 401.
-				if (errStatus.StatusCode == 405 && to.Username != "") || errStatus.StatusCode == 404 || errStatus.StatusCode == 401 {
+				// As of January 2022, ACR is known to return 400.
+				if (errStatus.StatusCode == 405 && to.Username != "") || errStatus.StatusCode == 404 || errStatus.StatusCode == 401 || errStatus.StatusCode == 400 {
 					resp, err := auth.FetchToken(ctx, ah.client, ah.header, to)
 					if err != nil {
 						return "", "", err
