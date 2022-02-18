a3303e72147f227827ed4d2f435fb080feb5ad5e
Kathryn Baldauf <kabaldau@microsoft.com>
2022-02-18 12:11:07 -0800

    Update TestContainerSymlinkVolumes to use windows path
    Signed-off-by: Kathryn Baldauf <kabaldau@microsoft.com>

--- integration/container_volume_test.go
+++ integration/container_volume_test.go
@@ -19,6 +19,7 @@ package integration
 import (
 	"os"
 	"path/filepath"
+	goruntime "runtime"
 	"testing"
 	"time"
 
@@ -103,9 +104,14 @@ func TestContainerSymlinkVolumes(t *testing.T) {
 			)
 
 			var (
-				testImage     = GetImage(BusyBox)
-				containerName = "test-container"
+				testImage          = GetImage(BusyBox)
+				containerName      = "test-container"
+				containerMountPath = "/mounted_file"
 			)
+
+			if goruntime.GOOS == "windows" {
+				containerMountPath = filepath.Clean("C:" + containerMountPath)
+			}
 
 			EnsureImageExists(t, testImage)
 
@@ -113,9 +119,9 @@ func TestContainerSymlinkVolumes(t *testing.T) {
 			cnConfig := ContainerConfig(
 				containerName,
 				testImage,
-				WithCommand("cat", "/mounted_file"),
+				WithCommand("cat", containerMountPath),
 				WithLogPath(containerName),
-				WithVolumeMount(file, "/mounted_file"),
+				WithVolumeMount(file, containerMountPath),
 			)
 
 			cn, err := runtimeService.CreateContainer(sb, cnConfig, sbConfig)
