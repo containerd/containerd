4e8bb624418c6a2848817a45760e605faa64a9a6
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2022-06-18 03:58:01 +0900

    DockerResolver: support immutable tags
    The tag part in `example.com/hello-world:42.0@sha256:deadbeef` had been just ignored.
    The tag was never verified with the specified digest because the tag was considered to be mutable.
    Even the existence of the tag was never verified, and it was causing a lot of confusions like issue 6450.
    
    This commit enables verification of the tag string that matches the `ImmutableTagMatcher` function.
    The matcher functions defaults to `HasSuffix(tag, "-immutable") || HasSuffix(tag, "_immutable") || HasSuffix(tag, ".immutable")`.
    
    e.g., `example.com/hello-world:42-immutable@sha256:deadbeef` is a ref string with an immutable tag.
    
    When the tag matches, the resolver always verifies the existence of the tag, and also verifies the digest
    if the digest is present in the ref string.
    
    i.e., pulling `example.com/hello-world:42-immutable@sha256:deadbeef` now fails if "42-immutable" does not exist
    or exists with an unexpected digest on `example.com/hello-world`.
    
    The matcher convention is being submitted to the OCI Distribution Spec in opencontainers/distribution-spec PR 320.
    
    Fixes issue 6450
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- remotes/docker/resolver.go
+++ remotes/docker/resolver.go
@@ -114,6 +114,10 @@ type ResolverOptions struct {
 	// Client is the http client to used when making registry requests
 	// Deprecated: use Hosts
 	Client *http.Client
+
+	// ImmutableTagMatcher returns true if the given tag string is immutable.
+	// Defaults to DefaultImmutableTagMatcher.
+	ImmutableTagMatcher ImmutableTagMatcher
 }
 
 // DefaultHost is the default host function.
@@ -124,11 +128,28 @@ func DefaultHost(ns string) (string, error) {
 	return ns, nil
 }
 
+// ImmutableTagMatcher returns true if the given tag string is immutable.
+type ImmutableTagMatcher func(string) bool
+
+// DefaultImmutableTagMatcher returns true if a tag contains one of the following suffixes:
+// "-immutable", "_immutable", or ".immutable" .
+//
+// See https://github.com/opencontainers/distribution-spec/pull/320
+var DefaultImmutableTagMatcher ImmutableTagMatcher = func(tag string) bool {
+	return strings.HasSuffix(tag, "-immutable") ||
+		strings.HasSuffix(tag, "_immutable") ||
+		strings.HasSuffix(tag, ".immutable")
+}
+
+// ErrImmutableTagWasMutated is returned when the immutable tag was resolved with an unexpected digest.
+var ErrImmutableTagWasMutated = errors.New("immutable tag was mutated")
+
 type dockerResolver struct {
-	hosts         RegistryHosts
-	header        http.Header
-	resolveHeader http.Header
-	tracker       StatusTracker
+	hosts               RegistryHosts
+	header              http.Header
+	resolveHeader       http.Header
+	tracker             StatusTracker
+	immutableTagMatcher ImmutableTagMatcher
 }
 
 // NewResolver returns a new resolver to a Docker registry
@@ -181,11 +202,16 @@ func NewResolver(options ResolverOptions) remotes.Resolver {
 		}
 		options.Hosts = ConfigureDefaultRegistries(opts...)
 	}
+	immutableTagMatcher := options.ImmutableTagMatcher
+	if immutableTagMatcher == nil {
+		immutableTagMatcher = DefaultImmutableTagMatcher
+	}
 	return &dockerResolver{
-		hosts:         options.Hosts,
-		header:        options.Headers,
-		resolveHeader: resolveHeader,
-		tracker:       options.Tracker,
+		hosts:               options.Hosts,
+		header:              options.Headers,
+		resolveHeader:       resolveHeader,
+		tracker:             options.Tracker,
+		immutableTagMatcher: immutableTagMatcher,
 	}
 }
 
@@ -228,11 +254,19 @@ func (r *dockerResolver) Resolve(ctx context.Context, ref string) (string, ocisp
 	}
 
 	var (
-		firstErr error
-		paths    [][]string
-		dgst     = refspec.Digest()
-		caps     = HostCapabilityPull
+		firstErr                    error
+		paths                       [][]string
+		refTagWithAtSymbol, refDgst = reference.SplitObject(refspec.Object)
+		immutableTag                bool
+		caps                        = HostCapabilityPull
 	)
+	refTag := strings.TrimSuffix(refTagWithAtSymbol, "@")
+	dgst := refDgst // dgst is altered later, while refDgst is not
+
+	if refTag != "" {
+		immutableTag = r.immutableTagMatcher(refTag)
+	}
+	log.G(ctx).WithField("refTag", refTag).WithField("refDgst", refDgst).Debugf("immutable tag: %v", immutableTag)
 
 	if dgst != "" {
 		if err := dgst.Validate(); err != nil {
@@ -241,11 +275,18 @@ func (r *dockerResolver) Resolve(ctx context.Context, ref string) (string, ocisp
 			return "", ocispec.Descriptor{}, err
 		}
 
-		// turns out, we have a valid digest, make a url.
-		paths = append(paths, []string{"manifests", dgst.String()})
+		if immutableTag {
+			// Look up the blob by the immutable tag, not by the digest.
+			// The digest of the found blob is compared with the ref digest at the end of the function.
+			paths = append(paths, []string{"manifests", refTag})
+			caps |= HostCapabilityResolve
+		} else {
+			// turns out, we have a valid digest, make a url.
+			paths = append(paths, []string{"manifests", dgst.String()})
 
-		// fallback to blobs on not found.
-		paths = append(paths, []string{"blobs", dgst.String()})
+			// fallback to blobs on not found.
+			paths = append(paths, []string{"blobs", dgst.String()})
+		}
 	} else {
 		// Add
 		paths = append(paths, []string{"manifests", refspec.Object})
@@ -310,7 +351,7 @@ func (r *dockerResolver) Resolve(ctx context.Context, ref string) (string, ocisp
 			// if no digest was provided, then only a resolve
 			// trusted registry was contacted, in this case use
 			// the digest header (or content from GET)
-			if dgst == "" {
+			if dgst == "" || immutableTag {
 				// this is the only point at which we trust the registry. we use the
 				// content headers to assemble a descriptor for the name. when this becomes
 				// more robust, we mostly get this information from a secure trust store.
@@ -378,6 +419,9 @@ func (r *dockerResolver) Resolve(ctx context.Context, ref string) (string, ocisp
 			}
 
 			log.G(ctx).WithField("desc.digest", desc.Digest).Debug("resolved")
+			if immutableTag && refDgst != "" && refDgst != dgst {
+				return "", ocispec.Descriptor{}, fmt.Errorf("immutable tag %q expects digest %q, got digest %q: %w", refTag, refDgst, dgst, ErrImmutableTagWasMutated)
+			}
 			return ref, desc, nil
 		}
 	}
--- remotes/docker/resolver_test.go
+++ remotes/docker/resolver_test.go
@@ -31,6 +31,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/containerd/containerd/errdefs"
 	"github.com/containerd/containerd/remotes"
 	"github.com/containerd/containerd/remotes/docker/auth"
 	digest "github.com/opencontainers/go-digest"
@@ -810,5 +811,100 @@ func (srv *refreshTokenServer) BasicTestFunc() func(h http.Handler) (string, Res
 			WithAuthorizer(authorizer),
 		)
 		return base, options, close
+	}
+}
+
+type httpHandlerWrapper struct {
+	http.Handler
+}
+
+func (h *httpHandlerWrapper) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	h.Handler.ServeHTTP(w, req)
+}
+
+func TestImmutableTags(t *testing.T) {
+	var (
+		ctx       = context.Background()
+		r         = http.NewServeMux()
+		imageName = strings.ToLower(t.Name())
+	)
+	const (
+		immutableTag   = "42.0-immutable"
+		nxMutableTag   = "non-existent-tag"
+		nxImmutableTag = "non-existent-tag.immutable"
+	)
+
+	m := newManifest(newContent(ocispec.MediaTypeImageConfig, []byte("content-for"+immutableTag)))
+	mc := newContent(ocispec.MediaTypeImageManifest, m.OCIManifest())
+	mcDigest := mc.Digest()
+	m.RegisterHandler(r, imageName)
+	r.Handle(fmt.Sprintf("/v2/%s/manifests/%s", imageName, mcDigest), mc)
+
+	immutableTagHandler := &httpHandlerWrapper{
+		Handler: mc,
+	}
+	r.Handle(fmt.Sprintf("/v2/%s/manifests/%s", imageName, immutableTag), immutableTagHandler)
+
+	base, ro, close := tlsServer(logHandler{t, r})
+	defer close()
+
+	resolver := NewResolver(ro)
+
+	// An immutabel tag with a digest is resolvable
+	refWithImmutableTagAndDigest := fmt.Sprintf("%s/%s:%s@%s", base, imageName, immutableTag, mc.Digest())
+	_, d, err := resolver.Resolve(ctx, refWithImmutableTagAndDigest)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if d.Digest != mcDigest {
+		t.Fatalf("expected %s, got %s", mcDigest, d.Digest)
+	}
+
+	// An immutabel tag is resolvable too, even if the digest is not specified
+	refWithImmutableTagButWithoutDigest := fmt.Sprintf("%s/%s:%s", base, imageName, immutableTag)
+	_, d, err = resolver.Resolve(ctx, refWithImmutableTagButWithoutDigest)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if d.Digest != mcDigest {
+		t.Fatalf("expected %s, got %s", mcDigest, d.Digest)
+	}
+
+	// A non-existent mutable tag is resolvable when the digest is specified
+	refWithNXMutableTagAndDigest := fmt.Sprintf("%s/%s:%s@%s", base, imageName, nxMutableTag, mc.Digest())
+	_, _, err = resolver.Resolve(ctx, refWithNXMutableTagAndDigest)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if d.Digest != mcDigest {
+		t.Fatalf("expected %s, got %s", mcDigest, d.Digest)
+	}
+
+	// A non-existent immutable tag is NOT resolvable, even when the digest is present
+	refWithNXImmutableTagAndDigest := fmt.Sprintf("%s/%s:%s@%s", base, imageName, nxImmutableTag, mc.Digest())
+	_, _, err = resolver.Resolve(ctx, refWithNXImmutableTagAndDigest)
+	if !errors.Is(err, errdefs.ErrNotFound) {
+		t.Fatalf("expected err to contain %v, got %v", errdefs.ErrNotFound, err)
+	}
+
+	// Mutate the content with the immutable tag, and make sure it is NOT resolvable when the old digest is specified
+	mMutated := newManifest(newContent(ocispec.MediaTypeImageConfig, []byte("mutated")))
+	mcMutated := newContent(ocispec.MediaTypeImageManifest, mMutated.OCIManifest())
+	mcMutatedDigest := mcMutated.Digest()
+	mMutated.RegisterHandler(r, imageName)
+	r.Handle(fmt.Sprintf("/v2/%s/manifests/%s", imageName, mcMutatedDigest), mcMutated)
+	immutableTagHandler.Handler = mcMutated
+	_, _, err = resolver.Resolve(ctx, refWithImmutableTagAndDigest)
+	if !errors.Is(err, ErrImmutableTagWasMutated) {
+		t.Fatalf("expected err to contain %v, got %v", ErrImmutableTagWasMutated, err)
+	}
+
+	// An immutabel tag is still resolvable (into the mutated content) when the digest is not specified
+	_, d, err = resolver.Resolve(ctx, refWithImmutableTagButWithoutDigest)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if d.Digest != mcMutatedDigest {
+		t.Fatalf("expected %s, got %s", mcMutatedDigest, d.Digest)
 	}
 }
