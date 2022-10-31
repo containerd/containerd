c607f83c3879d86264a0d5959abc185694e99dcb
Shiming Zhang <wzshiming@foxmail.com>
2022-10-31 17:18:27 +0800

    Fix missing closed fifo
    Signed-off-by: Shiming Zhang <wzshiming@foxmail.com>

--- runtime/v1/shim/service_linux.go
+++ runtime/v1/shim/service_linux.go
@@ -61,6 +61,7 @@ func (p *linuxPlatform) CopyConsole(ctx context.Context, console console.Console
 			// we need to shutdown epollConsole when pipe broken
 			epollConsole.Shutdown(p.epoller.CloseConsole)
 			epollConsole.Close()
+			in.Close()
 		}()
 	}
 
