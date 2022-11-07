903fc8f2ef31c1098763b20004037001d9e97f1e
Clarence "Sparr" Risher <clrnc@amazon.com>
2022-11-07 15:22:04 +0000

    Implement container lifecycle test

---
+++ integration/benchmark/benchmark_test.go
@@ -0,0 +1,116 @@
+package benchmark
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/cio"
+	"github.com/containerd/containerd/log/logtest"
+	"github.com/containerd/containerd/namespaces"
+	"github.com/containerd/containerd/oci"
+)
+
+var (
+	address       = "/run/containerd/containerd.sock"
+	testImage     = "docker.io/library/python:3"
+	testNamespace = "testing"
+)
+
+// Called once to set up and tear down for all the benchmarks in this file
+func TestMain(m *testing.M) {
+	// setup steps prior to all test runs
+	client, err := testClient(nil, address)
+	if err != nil {
+		fmt.Printf("Error testClient : %s\n", err)
+		return
+	}
+
+	ctx, cancel := testContext(nil)
+	defer cancel()
+
+	_, err = client.Pull(ctx, testImage, containerd.WithPullUnpack)
+	if err != nil {
+		fmt.Printf("Error Pull : %s\n", err)
+		return
+	}
+
+	// perform the actual benchmark runs
+	os.Exit(m.Run())
+
+	// teardown steps go here
+}
+
+func BenchmarkContainerLifecycle(b *testing.B) {
+	client, err := testClient(b, address)
+	if err != nil {
+		b.Fatalf("Error new client : %s\n", err)
+	}
+	defer client.Close()
+
+	ctx, cancel := testContext(b)
+	defer cancel()
+
+	image, err := client.GetImage(ctx, testImage)
+	if err != nil {
+		b.Fatalf("Error GetImage : %s\n", err)
+		return
+	}
+
+	container, err := client.NewContainer(ctx, "test-container",
+		containerd.WithImage(image),
+		containerd.WithNewSnapshot("test-snapshot", image),
+		containerd.WithNewSpec(
+			oci.WithImageConfig(image),
+			oci.WithProcessArgs("python3", "-c", "print('Hello World')"),
+		))
+	if err != nil {
+		b.Fatalf("Error NewContainer : %s\n", err)
+		return
+	}
+
+	defer container.Delete(ctx, containerd.WithSnapshotCleanup)
+
+	task, err := container.NewTask(ctx, cio.NewCreator(cio.WithStreams(nil, os.Stdout, os.Stderr)))
+	if err != nil {
+		b.Fatalf("Error NewTask : %s\n", err)
+		return
+	}
+
+	defer task.Delete(ctx)
+
+	exitC, err := task.Wait(ctx)
+	if err != nil {
+		b.Fatalf("Error Wait : %s\n", err)
+		return
+	}
+
+	err = task.Start(ctx)
+	if err != nil {
+		b.Fatalf("Error Start : %s\n", err)
+		return
+	}
+
+	<-exitC
+}
+
+func testClient(t testing.TB, address string, opts ...containerd.ClientOpt) (*containerd.Client, error) {
+	if rt := os.Getenv("TEST_RUNTIME"); rt != "" {
+		opts = append(opts, containerd.WithDefaultRuntime(rt))
+	}
+	if t != nil && testing.Short() {
+		t.Skip()
+	}
+	return containerd.New(address, opts...)
+}
+
+func testContext(t testing.TB) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+	ctx = namespaces.WithNamespace(ctx, testNamespace)
+	if t != nil {
+		ctx = logtest.WithT(ctx, t)
+	}
+	return ctx, cancel
+}
--- integration/benchmark/go.mod
+++ integration/benchmark/go.mod
@@ -2,11 +2,9 @@ module github.com/containerd/containerd/integration/benchmark
 
 go 1.18
 
-require (
-	github.com/containerd/containerd v1.5.13
-	github.com/hashicorp/go-multierror v1.1.1
-	github.com/montanaflynn/stats v0.6.6
-)
+require github.com/containerd/containerd v1.6.1
+
+require github.com/opencontainers/image-spec v1.0.3-0.20220303224323-02efb9a75ee1 // indirect
 
 require (
 	github.com/AdaLogics/go-fuzz-headers v0.0.0-20220706123152-fef3fe1bab07 // indirect
@@ -27,12 +25,12 @@ require (
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/klauspost/compress v1.11.13 // indirect
 	github.com/moby/locker v1.0.1 // indirect
 	github.com/moby/sys/mountinfo v0.6.2 // indirect
 	github.com/moby/sys/signal v0.7.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/opencontainers/image-spec v1.0.3-0.20220303224323-02efb9a75ee1 // indirect
 	github.com/opencontainers/runc v1.1.2 // indirect
 	github.com/opencontainers/runtime-spec v1.0.3-0.20210326190908-1c3f411f0417 // indirect
 	github.com/opencontainers/runtime-tools v0.0.0-20190417131837-cd1349b7c47e // indirect
