efd798563d54952952f02d5d3778e34ddd30f3db
Jian Zeng <anonymousknight96@gmail.com>
2023-01-04 21:16:36 +0800

    test: fix integration test
    Signed-off-by: Jian Zeng <anonymousknight96@gmail.com>

--- integration/client/transfer_test.go
+++ integration/client/transfer_test.go
@@ -45,7 +45,7 @@ func TestTransferEcho(t *testing.T) {
 func newImportExportEcho(ctx context.Context, client *containerd.Client, expected []byte) func(*testing.T) {
 	return func(t *testing.T) {
 		testBuf := newWaitBuffer()
-		err := client.Transfer(ctx, archive.NewImageImportStream(bytes.NewReader(expected), "application/octet-stream"), archive.NewImageExportStream(testBuf, "application/octet-stream"))
+		err := client.Transfer(ctx, archive.NewImageImportStream(bytes.NewReader(expected), "application/octet-stream"), archive.NewImageExportStream(testBuf, "application/octet-stream", archive.ExportOptions{}))
 		if err != nil {
 			t.Fatal(err)
 		}
