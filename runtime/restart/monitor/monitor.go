306cdefe1b58c213a12bccdd134ad95bcd5ea375
Iceber Gu <wei.cai-nat@daocloud.io>
2023-01-11 15:41:00 +0800

    Fix restarting the paused containers
    Signed-off-by: Iceber Gu <wei.cai-nat@daocloud.io>

--- runtime/restart/monitor/monitor.go
+++ runtime/restart/monitor/monitor.go
@@ -171,9 +171,15 @@ func (m *monitor) monitor(ctx context.Context) ([]change, error) {
 		// which will result in an `on-failure` restart policy reconcile error.
 		switch desiredStatus {
 		case containerd.Running:
+			switch status.Status {
+			case containerd.Paused, containerd.Pausing:
+				continue
+			default:
+			}
 			if !restart.Reconcile(status, labels) {
 				continue
 			}
+
 			restartCount, _ := strconv.Atoi(labels[restart.CountLabel])
 			if labels["containerd.io/restart.logpath"] != "" {
 				logrus.Warn(`Label "containerd.io/restart.logpath" is no longer supported since containerd v2.0. Use "containerd.io/restart.loguri" instead.`)
