566feae63e8fa07a97ee38a8f9f0d9363ef45195
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2023-01-11 13:12:25 -0800

    Have separate spec builder for each platform
    Signed-off-by: Maksym Pavlenko <pavlenko.maksym@gmail.com>

--- pkg/cri/sbserver/container_create_other.go
+++ pkg/cri/sbserver/container_create_other.go
@@ -19,27 +19,17 @@
 package sbserver
 
 import (
-	"github.com/containerd/containerd/oci"
-	"github.com/containerd/containerd/snapshots"
 	imagespec "github.com/opencontainers/image-spec/specs-go/v1"
 	runtime "k8s.io/cri-api/pkg/apis/runtime/v1"
+
+	"github.com/containerd/containerd/oci"
+	"github.com/containerd/containerd/snapshots"
 )
 
 // containerMounts sets up necessary container system file mounts
 // including /dev/shm, /etc/hosts and /etc/resolv.conf.
 func (c *criService) containerMounts(sandboxID string, config *runtime.ContainerConfig) []*runtime.Mount {
 	return []*runtime.Mount{}
-}
-
-func (c *criService) platformSpec(
-	id string,
-	sandboxID string,
-	config *runtime.ContainerConfig,
-	sandboxConfig *runtime.PodSandboxConfig,
-	imageConfig *imagespec.ImageConfig,
-	extraMounts []*runtime.Mount,
-) ([]oci.SpecOpts, error) {
-	return []oci.SpecOpts{}, nil
 }
 
 func (c *criService) containerSpecOpts(config *runtime.ContainerConfig, imageConfig *imagespec.ImageConfig) ([]oci.SpecOpts, error) {
