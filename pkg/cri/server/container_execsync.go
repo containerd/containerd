5946c1051ed84b4bc3a5e3bb98f516b1dd648f3d
Wei Fu <fuweid89@gmail.com>
2023-03-03 17:51:03 +0800

    *: fix code style issue
    1. it's easy to check wrong input if using drain_exec_sync_io_timeout in error
    2. avoid to use full error message, as part of error generated by go
       stdlib would be changed in the future
    3. delete the extra empty line
    
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

--- pkg/cri/server/container_execsync.go
+++ pkg/cri/server/container_execsync.go
@@ -290,7 +290,6 @@ func drainExecSyncIO(ctx context.Context, execProcess containerd.Process, drainE
 
 	select {
 	case <-timerCh:
-
 	case <-attachDone:
 		log.G(ctx).Debugf("Stream pipe for exec process %q done", execProcess.ID())
 		return nil
