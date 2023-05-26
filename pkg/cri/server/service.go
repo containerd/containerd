bccaf68b7749b62d821b80c8a845417113f44310
Henry Wang <henwang@amazon.com>
2023-05-26 03:07:40 +0000

    notify readiness when registered plugins are ready
    Signed-off-by: Henry Wang <henwang@amazon.com>
    (cherry picked from commit 4bfcac85fa47d0065f969249651c0ffb2c6b6810)

--- pkg/cri/server/service.go
+++ pkg/cri/server/service.go
@@ -66,7 +66,7 @@ type grpcAlphaServices interface {
 
 // CRIService is the interface implement CRI remote service server.
 type CRIService interface {
-	Run() error
+	Run(ready func()) error
 	// io.Closer is used by containerd to gracefully stop cri service.
 	io.Closer
 	Register(*grpc.Server) error
@@ -200,7 +200,7 @@ func (c *criService) RegisterTCP(s *grpc.Server) error {
 }
 
 // Run starts the CRI service.
-func (c *criService) Run() error {
+func (c *criService) Run(ready func()) error {
 	logrus.Info("Start subscribing containerd event")
 	c.eventMonitor.subscribe(c.client)
 
@@ -251,6 +251,7 @@ func (c *criService) Run() error {
 
 	// Set the server as initialized. GRPC services could start serving traffic.
 	c.initialized.Set()
+	ready()
 
 	var eventMonitorErr, streamServerErr, cniNetConfMonitorErr error
 	// Stop the whole CRI service if any of the critical service exits.
