da56afe3f31a2b835bdcbda906d1e939d1546e48
ningmingxiao <ning.mingxiao@zte.com.cn>
2023-05-29 22:47:46 +0800

    fix exec slow when use concurrent execs
    Signed-off-by: ningmingxiao <ning.mingxiao@zte.com.cn>

--- sys/reaper/reaper_unix.go
+++ sys/reaper/reaper_unix.go
@@ -34,7 +34,7 @@ import (
 // ErrNoSuchProcess is returned when the process no longer exists
 var ErrNoSuchProcess = errors.New("no such process")
 
-const bufferSize = 32
+const bufferSize = 1024
 
 type subscriber struct {
 	sync.Mutex
