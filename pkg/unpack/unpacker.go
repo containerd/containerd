9aafe4d1de1ca29fd6fff25fa26118110d505175
Simon Kaegi <simon.kaegi@gmail.com>
2023-07-26 11:06:53 -0400

    Unpacks should check and  always do fetch if content not present
    This patch...
    1) creates doFetchFn from existing fetch logic.
    2) After the sn.Stat check we also check cs.Info and if not found will call doFetchFn.
    3) Existing fetch later in this function is unchanged but instead calls doFetchFn.
    
    Signed-off-by: Simon Kaegi simon_kaegi@ca.ibm.com

--- pkg/unpack/unpacker.go
+++ pkg/unpack/unpacker.go
@@ -284,6 +284,36 @@ func (u *Unpacker) unpack(
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
+	doFetchFn := func(i int) error {
+		if fetchErr == nil {
+			fetchErr = make(chan error, 1)
+			fetchOffset = i
+			fetchC = make([]chan struct{}, len(layers)-fetchOffset)
+			for i := range fetchC {
+				fetchC[i] = make(chan struct{})
+			}
+
+			go func(i int) {
+				err := u.fetch(ctx, h, layers[i:], fetchC)
+				if err != nil {
+					fetchErr <- err
+				}
+				close(fetchErr)
+			}(i)
+		}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case err := <-fetchErr:
+			if err != nil {
+				return err
+			}
+		case <-fetchC[i-fetchOffset]:
+		}
+		return nil
+	}
+
 	doUnpackFn := func(i int, desc ocispec.Descriptor) error {
 		parent := identity.ChainID(chain)
 		chain = append(chain, diffIDs[i])
@@ -296,8 +326,14 @@ func (u *Unpacker) unpack(
 		defer unlock()
 
 		if _, err := sn.Stat(ctx, chainID); err == nil {
-			// no need to handle
-			return nil
+			if _, err := cs.Info(ctx, desc.Digest); err == nil {
+				// no need to handle
+				return nil
+			} else if errdefs.IsNotFound(err) {
+				return doFetchFn(i)
+			} else {
+				return fmt.Errorf("failed to get content info %s: %w", desc.Digest.String(), err)
+			}
 		} else if !errdefs.IsNotFound(err) {
 			return fmt.Errorf("failed to stat snapshot %s: %w", chainID, err)
 		}
@@ -349,33 +385,9 @@ func (u *Unpacker) unpack(
 			}
 		}
 
-		if fetchErr == nil {
-			fetchErr = make(chan error, 1)
-			fetchOffset = i
-			fetchC = make([]chan struct{}, len(layers)-fetchOffset)
-			for i := range fetchC {
-				fetchC[i] = make(chan struct{})
-			}
-
-			go func(i int) {
-				err := u.fetch(ctx, h, layers[i:], fetchC)
-				if err != nil {
-					fetchErr <- err
-				}
-				close(fetchErr)
-			}(i)
-		}
-
-		select {
-		case <-ctx.Done():
+		if err := doFetchFn(i); err != nil {
 			cleanup.Do(ctx, abort)
-			return ctx.Err()
-		case err := <-fetchErr:
-			if err != nil {
-				cleanup.Do(ctx, abort)
-				return err
-			}
-		case <-fetchC[i-fetchOffset]:
+			return err
 		}
 
 		diff, err := a.Apply(ctx, desc, mounts, unpack.ApplyOpts...)
