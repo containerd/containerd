bb66b6b275312a61df912313d11898b96de3d815
Simon Kaegi <simon.kaegi@gmail.com>
2023-07-26 12:38:24 -0400

    Unpacks should check and  always do fetch if content not present
    This patch moves the content check and fetch to the start of doUnpackFn. This means that we "always" will check for content even if the snapshot diffid is unpacked already and download the content if missing.
    
    This handles a real world case where image content has a different digest but the same diff id, which result in an incomplete image and then problems exporting images or using the image with a different snapshotter.
    
    Signed-off-by: Simon Kaegi <simon.kaegi@gmail.com>

--- pkg/unpack/unpacker.go
+++ pkg/unpack/unpacker.go
@@ -285,6 +285,41 @@ func (u *Unpacker) unpack(
 	defer cancel()
 
 	doUnpackFn := func(i int, desc ocispec.Descriptor) error {
+		// always perform content check and fetch if not present
+		// this prevents the sn.Stat chain id check from returning without downloading the content
+		if _, err := cs.Info(ctx, desc.Digest); err != nil {
+			if !errdefs.IsNotFound(err) {
+				return fmt.Errorf("failed to get content info %s: %w", desc.Digest.String(), err)
+			}
+
+			if fetchErr == nil {
+				fetchErr = make(chan error, 1)
+				fetchOffset = i
+				fetchC = make([]chan struct{}, len(layers)-fetchOffset)
+				for i := range fetchC {
+					fetchC[i] = make(chan struct{})
+				}
+
+				go func(i int) {
+					err := u.fetch(ctx, h, layers[i:], fetchC)
+					if err != nil {
+						fetchErr <- err
+					}
+					close(fetchErr)
+				}(i)
+			}
+
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case err := <-fetchErr:
+				if err != nil {
+					return err
+				}
+			case <-fetchC[i-fetchOffset]:
+			}
+		}
+
 		parent := identity.ChainID(chain)
 		chain = append(chain, diffIDs[i])
 		chainID := identity.ChainID(chain).String()
@@ -347,35 +382,6 @@ func (u *Unpacker) unpack(
 			if err := sn.Remove(ctx, key); err != nil {
 				log.G(ctx).WithError(err).Errorf("failed to cleanup %q", key)
 			}
-		}
-
-		if fetchErr == nil {
-			fetchErr = make(chan error, 1)
-			fetchOffset = i
-			fetchC = make([]chan struct{}, len(layers)-fetchOffset)
-			for i := range fetchC {
-				fetchC[i] = make(chan struct{})
-			}
-
-			go func(i int) {
-				err := u.fetch(ctx, h, layers[i:], fetchC)
-				if err != nil {
-					fetchErr <- err
-				}
-				close(fetchErr)
-			}(i)
-		}
-
-		select {
-		case <-ctx.Done():
-			cleanup.Do(ctx, abort)
-			return ctx.Err()
-		case err := <-fetchErr:
-			if err != nil {
-				cleanup.Do(ctx, abort)
-				return err
-			}
-		case <-fetchC[i-fetchOffset]:
 		}
 
 		diff, err := a.Apply(ctx, desc, mounts, unpack.ApplyOpts...)
