3d27bc738a6e6762a9c240f420491632e3eaae3d
Aditya Ramani <a_ramani@apple.com>
2023-09-18 11:52:17 -0700

    Handle unexpected shim kill events
    When a shim process is unexpectedly killed in a way that was not initiated through containerd - containerd reports the pod as not ready but the containers as running. This results in kubelet repeatedly sending container kill requests that fail since containerd cannot connect to the shim.
    
    Changes:
    
    - In the container exit handler, treat `err: Unavailable` as if the container has already exited out
    - When attempting to get a connection to the shim, if the controller isn't available assume that the shim has been killed (needs to be done since we have a separate exit handler that cleans up the reference to the shim controller - before kubelet has the chance to call StopPodSandbox)
    
    Signed-off-by: Aditya Ramani <a_ramani@apple.com>
    (cherry picked from commit 729c97cf39121fc949fcf877defe7c68103c1dfc)
    Signed-off-by: Danny Canter <danny@dcantah.dev>

--- plugins/sandbox/controller.go
+++ plugins/sandbox/controller.go
@@ -260,6 +260,12 @@ func (c *controllerLocal) Wait(ctx context.Context, sandboxID string) (sandbox.E
 
 func (c *controllerLocal) Status(ctx context.Context, sandboxID string, verbose bool) (sandbox.ControllerStatus, error) {
 	svc, err := c.getSandbox(ctx, sandboxID)
+	if errdefs.IsNotFound(err) {
+		return sandbox.ControllerStatus{
+			SandboxID: sandboxID,
+			ExitedAt:  time.Now(),
+		}, nil
+	}
 	if err != nil {
 		return sandbox.ControllerStatus{}, err
 	}
@@ -286,7 +292,7 @@ func (c *controllerLocal) Status(ctx context.Context, sandboxID string, verbose 
 func (c *controllerLocal) getSandbox(ctx context.Context, id string) (runtimeAPI.TTRPCSandboxService, error) {
 	shim, err := c.shims.Get(ctx, id)
 	if err != nil {
-		return nil, errdefs.ErrNotFound
+		return nil, err
 	}
 
 	return sandbox.NewClient(shim.Client())
