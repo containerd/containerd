106a9b776778eea9e1485f5340277dcd900590b9
Bjorn Neergaard <bjorn.neergaard@docker.com>
2023-09-18 16:56:11 -0600

    oci/spec: deny /sys/devices/virtual/powercap
    The ability to read these files may offer a power-based sidechannel
    attack against any workloads running on the same kernel.
    
    This was originally [CVE-2020-8694][1], which was fixed in
    [949dd0104c496fa7c14991a23c03c62e44637e71][2] by restricting read access
    to root. However, since many containers run as root, this is not
    sufficient for our use case.
    
    While untrusted code should ideally never be run, we can add some
    defense in depth here by masking out the device class by default.
    
    [Other mechanisms][3] to access this hardware exist, but they should not
    be accessible to a container due to other safeguards in the
    kernel/container stack (e.g. capabilities, perf paranoia).
    
    [1]: https://nvd.nist.gov/vuln/detail/CVE-2020-8694
    [2]: https://github.com/torvalds/linux/commit/949dd0104c496fa7c14991a23c03c62e44637e71
    [3]: https://web.eece.maine.edu/~vweaver/projects/rapl/
    
    Signed-off-by: Bjorn Neergaard <bjorn.neergaard@docker.com>

--- oci/spec.go
+++ oci/spec.go
@@ -196,6 +196,7 @@ func populateDefaultUnixSpec(ctx context.Context, s *Spec, id string) error {
 				"/proc/timer_stats",
 				"/proc/sched_debug",
 				"/sys/firmware",
+				"/sys/devices/virtual/powercap",
 				"/proc/scsi",
 			},
 			ReadonlyPaths: []string{
