ed5fa63bc0c6b8a92a9b4bb17df15e169d4b5a08
Charity Kathure <ckathure@microsoft.com>
2023-09-19 15:45:12 +0300

    fix windows default path overwrite issue

--- oci/spec_opts.go
+++ oci/spec_opts.go
@@ -190,7 +190,9 @@ func WithEnv(environmentVariables []string) SpecOpts {
 // WithDefaultPathEnv sets the $PATH environment variable to the
 // default PATH defined in this package.
 func WithDefaultPathEnv(_ context.Context, _ Client, _ *containers.Container, s *Spec) error {
-	s.Process.Env = replaceOrAppendEnvValues(s.Process.Env, defaultUnixEnv)
+	if s.Linux != nil {
+		s.Process.Env = replaceOrAppendEnvValues(s.Process.Env, defaultUnixEnv)
+	}
 	return nil
 }
 
