25a4c3d2355b7c1a3b7df4be370c7f72d71cb386
Abel Feng <fshb1988@gmail.com>
2023-10-20 09:48:59 +0800

    sandbox: remove SandboxersServicePlugin
    Signed-off-by: Abel Feng <fshb1988@gmail.com>

--- plugins/types.go
+++ plugins/types.go
@@ -67,8 +67,6 @@ const (
 	ImageVerifierPlugin plugin.Type = "io.containerd.image-verifier.v1"
 	// WarningPlugin implements a warning service
 	WarningPlugin plugin.Type = "io.containerd.warning.v1"
-	// SandboxesServicePlugin implements sandboxes service
-	SandboxesServicePlugin plugin.Type = "io.containerd.service.sandboxes.v1"
 )
 
 const (
