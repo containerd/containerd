821916b7e6d53b259898780a7f26c799fcb581b0
Milas Bowman <milas.bowman@docker.com>
2023-11-27 17:28:12 -0500

    fix(tracing): use latest version of semconv
    All components need to use a consistent `semconv` version or OTel
    will emit errors about "cannot merge resource due to conflicting Schema URL".
    
    Switch to the appropriate semconv version, which requires dropping
    usage of `httpconv`. Instead, the upstream HTTP client hooks are
    used directly. (The lower-level functions are no longer exported by
    OTel.)
    
    Signed-off-by: Milas Bowman <milas.bowman@docker.com>

--- tracing/plugin/otlp.go
+++ tracing/plugin/otlp.go
@@ -35,7 +35,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 )
 
 const exporterPlugin = "otlp"
