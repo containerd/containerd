86bd4794c7f1d73cd55a58edebb8644b50c68c33
Derek McGowan <derek@mcg.dev>
2024-01-17 09:56:34 -0800

    Move pkg/cleanup to internal/cleanup
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- runtime/v2/shim_load.go
+++ runtime/v2/shim_load.go
@@ -23,7 +23,7 @@ import (
 	"path/filepath"
 
 	"github.com/containerd/containerd/v2/core/mount"
-	"github.com/containerd/containerd/v2/pkg/cleanup"
+	"github.com/containerd/containerd/v2/internal/cleanup"
 	"github.com/containerd/containerd/v2/pkg/errdefs"
 	"github.com/containerd/containerd/v2/pkg/namespaces"
 	"github.com/containerd/containerd/v2/pkg/timeout"
