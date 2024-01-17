f8cdf0eaf6002b2cda848d2254a77412b20fd3f7
Derek McGowan <derek@mcg.dev>
2024-01-17 09:56:43 -0800

    Move pkg/failpoint to internal/failpoint
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- integration/sandbox_run_rollback_test.go
+++ integration/sandbox_run_rollback_test.go
@@ -35,8 +35,8 @@ import (
 	"github.com/stretchr/testify/require"
 	criapiv1 "k8s.io/cri-api/pkg/apis/runtime/v1"
 
+	"github.com/containerd/containerd/v2/internal/failpoint"
 	"github.com/containerd/containerd/v2/pkg/cri/server/base"
-	"github.com/containerd/containerd/v2/pkg/failpoint"
 )
 
 const (
