4982d5093b36c2197a4754e6d447c58241d1d98c
Derek McGowan <derek@mcg.dev>
2024-01-17 09:57:10 -0800

    Move pkg/randutil to internal/randutil
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- core/snapshots/testsuite/testsuite.go
+++ core/snapshots/testsuite/testsuite.go
@@ -30,9 +30,9 @@ import (
 
 	"github.com/containerd/containerd/v2/core/mount"
 	"github.com/containerd/containerd/v2/core/snapshots"
+	"github.com/containerd/containerd/v2/internal/randutil"
 	"github.com/containerd/containerd/v2/pkg/errdefs"
 	"github.com/containerd/containerd/v2/pkg/namespaces"
-	"github.com/containerd/containerd/v2/pkg/randutil"
 	"github.com/containerd/containerd/v2/pkg/testutil"
 	"github.com/containerd/continuity/fs/fstest"
 	"github.com/containerd/log/logtest"
