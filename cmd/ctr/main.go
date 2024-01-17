881ccb64e656241d405a0abc204e6f5696d2ce9b
Derek McGowan <derek@mcg.dev>
2024-01-17 09:56:52 -0800

    Move pkg/hasher to internal/hasher
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- cmd/ctr/main.go
+++ cmd/ctr/main.go
@@ -22,7 +22,7 @@ import (
 	"os"
 
 	"github.com/containerd/containerd/v2/cmd/ctr/app"
-	"github.com/containerd/containerd/v2/pkg/hasher"
+	"github.com/containerd/containerd/v2/internal/hasher"
 	"github.com/containerd/containerd/v2/pkg/seed" //nolint:staticcheck // Global math/rand seed is deprecated, but still used by external dependencies
 	"github.com/urfave/cli"
 )
