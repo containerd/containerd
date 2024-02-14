99fa35e7015d487c210d82862b4c807a913d50f2
Kirtana Ashok <kiashok@microsoft.com>
2024-02-13 20:05:50 -0800

    Move high volume event logs to Trace level
    Signed-off-by: Kirtana Ashok <kiashok@microsoft.com>
    (cherry picked from commit 4dcf089fa30ebc3a733edbaedc79130c21a9b887)
    Signed-off-by: Kirtana Ashok <kiashok@microsoft.com>

--- events/exchange/exchange.go
+++ events/exchange/exchange.go
@@ -69,7 +69,7 @@ func (e *Exchange) Forward(ctx context.Context, envelope *events.Envelope) (err 
 		if err != nil {
 			logger.WithError(err).Error("error forwarding event")
 		} else {
-			logger.Debug("event forwarded")
+			logger.Trace("event forwarded")
 		}
 	}()
 
@@ -114,7 +114,7 @@ func (e *Exchange) Publish(ctx context.Context, topic string, event events.Event
 		if err != nil {
 			logger.WithError(err).Error("error publishing event")
 		} else {
-			logger.Debug("event published")
+			logger.Trace("event published")
 		}
 	}()
 
