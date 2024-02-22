464319b3ab07c96a5e8a7c3578366098e3a64e8d
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2024-02-22 11:42:16 -0800

    Remove pause package from runc shim
    Signed-off-by: Maksym Pavlenko <pavlenko.maksym@gmail.com>

--- cmd/containerd-shim-runc-v2/main.go
+++ cmd/containerd-shim-runc-v2/main.go
@@ -22,7 +22,6 @@ import (
 	"context"
 
 	"github.com/containerd/containerd/v2/cmd/containerd-shim-runc-v2/manager"
-	_ "github.com/containerd/containerd/v2/cmd/containerd-shim-runc-v2/pause"
 	_ "github.com/containerd/containerd/v2/cmd/containerd-shim-runc-v2/task/plugin"
 	"github.com/containerd/containerd/v2/core/runtime/v2/shim"
 )
