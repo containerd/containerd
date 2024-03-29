2d5689434d65f9d415d3aaefcea13800ce5421d2
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2024-03-29 22:45:03 +0900

    CI: use Go 1.22 by default
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- script/setup/prepare_env_windows.ps1
+++ script/setup/prepare_env_windows.ps1
@@ -5,7 +5,7 @@
 # lived test environment.
 Set-MpPreference -DisableRealtimeMonitoring:$true
 
-$PACKAGES= @{ mingw = "10.2.0"; git = ""; golang = "1.21.9"; make = ""; nssm = "" }
+$PACKAGES= @{ mingw = "10.2.0"; git = ""; golang = "1.22.2"; make = ""; nssm = "" }
 
 Write-Host "Downloading chocolatey package"
 curl.exe -L "https://packages.chocolatey.org/chocolatey.0.10.15.nupkg" -o 'c:\choco.zip'
