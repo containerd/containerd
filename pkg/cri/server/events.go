aca1ca44060500f76804b6378d28a9ba4a648a34
ningmingxiao <ning.mingxiao@zte.com.cn>
2024-04-01 19:42:45 +0800

    add info of exited event
    Signed-off-by: ningmingxiao <ning.mingxiao@zte.com.cn>
    (cherry picked from commit 7bd4d348ebf6becfd4cc8d344819ffb925026c0c)
    Signed-off-by: ningmingxiao <ning.mingxiao@zte.com.cn>

--- pkg/cri/server/events.go
+++ pkg/cri/server/events.go
@@ -128,7 +128,7 @@ func (em *eventMonitor) startSandboxExitMonitor(ctx context.Context, id string, 
 				ExitedAt:    exitedAt,
 			}
 
-			logrus.Debugf("received exit event %+v", e)
+			logrus.Infof("received exit event %+v", e)
 
 			err = func() error {
 				dctx := ctrdutil.NamespacedContext()
@@ -179,7 +179,7 @@ func (em *eventMonitor) startContainerExitMonitor(ctx context.Context, id string
 				ExitedAt:    exitedAt,
 			}
 
-			logrus.Debugf("received exit event %+v", e)
+			logrus.Infof("received exit event %+v", e)
 
 			err = func() error {
 				dctx := ctrdutil.NamespacedContext()
