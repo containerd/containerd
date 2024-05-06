f0363a7f6b626731b5c960482f26bd01c1b69fda
Danny Canter <danny@dcantah.dev>
2024-05-06 00:44:13 -0700

    Chore: Simplify some syscall error checks
    This just replaces some type casts to check whether a few dial errors are
    a specific syscall with the stdlibs errors.As/errors.Is pals.
    
    Signed-off-by: Danny Canter <danny@dcantah.dev>

--- pkg/dialer/dialer_unix.go
+++ pkg/dialer/dialer_unix.go
@@ -19,9 +19,9 @@
 package dialer
 
 import (
+	"errors"
 	"fmt"
 	"net"
-	"os"
 	"strings"
 	"syscall"
 	"time"
@@ -34,16 +34,7 @@ func DialAddress(address string) string {
 }
 
 func isNoent(err error) bool {
-	if err != nil {
-		if nerr, ok := err.(*net.OpError); ok {
-			if serr, ok := nerr.Err.(*os.SyscallError); ok {
-				if serr.Err == syscall.ENOENT {
-					return true
-				}
-			}
-		}
-	}
-	return false
+	return errors.Is(err, syscall.ENOENT)
 }
 
 func dialer(address string, timeout time.Duration) (net.Conn, error) {
