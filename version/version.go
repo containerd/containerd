288f0592e5a465e696837f193c25183c826f4cf4
Derek McGowan <derek@mcg.dev>
2024-05-10 22:33:18 -0700

    Prepare release notes for v2.0.0-rc.2
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- version/version.go
+++ version/version.go
@@ -23,7 +23,7 @@ var (
 	Package = "github.com/containerd/containerd/v2"
 
 	// Version holds the complete version number. Filled in at linking time.
-	Version = "2.0.0-rc.1+unknown"
+	Version = "2.0.0-rc.2+unknown"
 
 	// Revision is filled with the VCS (e.g. git) revision being used to build
 	// the program at linking time.
