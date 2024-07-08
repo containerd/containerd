131defa5fd92c1c43fed75de0debf3dcbf1a0200
Kirtana Ashok <kiashok@microsoft.com>
2024-07-08 11:17:22 -0700

    Support image pull per runtime class
    Adds support for image pull per runtime
    class with the help of gc labels.
    The image pull code path remains the same
    but during UpdateImage() call, the image index
    or manifest is walked to find the appropriate
    platform, snapshot the image was unpacked for.
    If such a match if found, find all matching
    runtime handlers with identical platform and
    snapshotter from CriConfig.RuntimePlatforms and
    update containerd and cri image stores appropriately.
    New entry for tuple (ref,runtimehandler) are made in
    containerd metadata store and snapshot gc labels are
    added to take reference on unpacked snapshot.
    Further, image gc labels are also added to the root image
    to ensure appropriate resource cleanup.
    This approach also makes the image delete and reload code
    paths simple.
    
    Signed-off-by: Kirtana Ashok <kiashok@microsoft.com>

--- internal/cri/server/container_status_test.go
+++ internal/cri/server/container_status_test.go
@@ -28,6 +28,7 @@ import (
 	snapshotstore "github.com/containerd/containerd/v2/internal/cri/store/snapshot"
 	"github.com/containerd/containerd/v2/pkg/cio"
 	"github.com/containerd/typeurl/v2"
+	imagespec "github.com/opencontainers/image-spec/specs-go/v1"
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/stretchr/testify/assert"
 	runtime "k8s.io/cri-api/pkg/apis/runtime/v1"
@@ -290,17 +291,23 @@ func (s *fakeImageService) UpdateImage(ctx context.Context, r string) error { re
 
 func (s *fakeImageService) CheckImages(ctx context.Context) error { return nil }
 
-func (s *fakeImageService) GetImage(id string) (imagestore.Image, error) { return s.imageStore.Get(id) }
+func (s *fakeImageService) GetImage(id string, runtimeHandler string) (imagestore.Image, error) {
+	return s.imageStore.Get(id, runtimeHandler)
+}
 
 func (s *fakeImageService) GetSnapshot(key, snapshotter string) (snapshotstore.Snapshot, error) {
 	return snapshotstore.Snapshot{}, errors.New("not implemented")
 }
 
-func (s *fakeImageService) LocalResolve(refOrID string) (imagestore.Image, error) {
+func (s *fakeImageService) LocalResolve(refOrID string, runtimeHandler string) (imagestore.Image, error) {
 	return imagestore.Image{}, errors.New("not implemented")
 }
 
 func (s *fakeImageService) ImageFSPaths() map[string]string { return make(map[string]string) }
+
+func (s *fakeImageService) PlatformForRuntimeHandler(runtimeHandler string) imagespec.Platform {
+	return imagespec.Platform{}
+}
 
 func (s *fakeImageService) PullImage(context.Context, string, func(string) (string, string, error), *runtime.PodSandboxConfig, string) (string, error) {
 	return "", errors.New("not implemented")
