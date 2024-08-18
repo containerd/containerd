74517db62a2d288f530391ea635c21f7d25e6dd7
Jian Zeng <anonymousknight96@gmail.com>
2024-08-19 00:30:34 +0800

    feat: set credentials in host configurations
    Now users are able to set credentials and domain alias for the mirror.
    
    For instance, with the following config:
    ```
    [host."https://harbor.infra.mydomain.com/v2/quay.io"]
      capabilities = ["pull", "resolve"]
      override_path = true
      username = "..."
      password = "..."
      credential_domain = "quay.io"
    ```
    containerd will send the credentials to this mirror.
    
    Signed-off-by: Jian Zeng <anonymousknight96@gmail.com>

--- core/remotes/docker/config/hosts.go
+++ core/remotes/docker/config/hosts.go
@@ -54,7 +54,10 @@ type hostConfig struct {
 
 	header http.Header
 
-	// TODO: Add credential configuration (domain alias, username)
+	// TODO: Add credential configuration (token, helper, ...)
+	credentialDomain string
+	username         string
+	password         string
 }
 
 // HostOptions is used to configure registry hosts
@@ -68,15 +71,42 @@ type HostOptions struct {
 	AuthorizerOpts []docker.AuthorizerOpt
 }
 
+func authCredentialFromHostConfig(hostNamespace string, hostCfg hostConfig) func(string) (string, string, error) {
+	return func(string) (string, string, error) {
+		if hostCfg.credentialDomain == "" || hostCfg.credentialDomain == hostNamespace {
+			return hostCfg.username, hostCfg.password, nil
+		}
+		return "", "", nil
+	}
+}
+
+func compositeAuthCredentials(auths ...func(string) (string, string, error)) func(string) (string, string, error) {
+	return func(host string) (string, string, error) {
+		for _, auth := range auths {
+			if auth == nil {
+				continue
+			}
+			username, password, err := auth(host)
+			if err != nil {
+				return "", "", fmt.Errorf("get credential: %w", err)
+			}
+			if username != "" || password != "" {
+				return username, password, nil
+			}
+		}
+		return "", "", nil
+	}
+}
+
 // ConfigureHosts creates a registry hosts function from the provided
 // host creation options. The host directory can read hosts.toml or
 // certificate files laid out in the Docker specific layout.
 // If a `HostDir` function is not required, defaults are used.
 func ConfigureHosts(ctx context.Context, options HostOptions) docker.RegistryHosts {
-	return func(host string) ([]docker.RegistryHost, error) {
+	return func(hostNamespace string) ([]docker.RegistryHost, error) {
 		var hosts []hostConfig
 		if options.HostDir != nil {
-			dir, err := options.HostDir(host)
+			dir, err := options.HostDir(hostNamespace)
 			if err != nil && !errdefs.IsNotFound(err) {
 				return nil, err
 			}
@@ -96,13 +126,13 @@ func ConfigureHosts(ctx context.Context, options HostOptions) docker.RegistryHos
 			hosts = make([]hostConfig, 1)
 		}
 		if len(hosts) > 0 && hosts[len(hosts)-1].host == "" {
-			if host == "docker.io" {
+			if hostNamespace == "docker.io" {
 				hosts[len(hosts)-1].scheme = "https"
 				hosts[len(hosts)-1].host = "registry-1.docker.io"
-			} else if docker.IsLocalhost(host) {
-				hosts[len(hosts)-1].host = host
+			} else if docker.IsLocalhost(hostNamespace) {
+				hosts[len(hosts)-1].host = hostNamespace
 				if options.DefaultScheme == "" {
-					_, port, _ := net.SplitHostPort(host)
+					_, port, _ := net.SplitHostPort(hostNamespace)
 					if port == "" || port == "443" {
 						// If port is default or 443, only use https
 						hosts[len(hosts)-1].scheme = "https"
@@ -121,7 +151,7 @@ func ConfigureHosts(ctx context.Context, options HostOptions) docker.RegistryHos
 					hosts[len(hosts)-1].scheme = options.DefaultScheme
 				}
 			} else {
-				hosts[len(hosts)-1].host = host
+				hosts[len(hosts)-1].host = hostNamespace
 				if options.DefaultScheme != "" {
 					hosts[len(hosts)-1].scheme = options.DefaultScheme
 				} else {
@@ -167,17 +197,14 @@ func ConfigureHosts(ctx context.Context, options HostOptions) docker.RegistryHos
 			}
 		}
 
-		authOpts := []docker.AuthorizerOpt{docker.WithAuthClient(client)}
-		if options.Credentials != nil {
-			authOpts = append(authOpts, docker.WithAuthCreds(options.Credentials))
-		}
-		authOpts = append(authOpts, options.AuthorizerOpts...)
-		authorizer := docker.NewDockerAuthorizer(authOpts...)
+		authOpts := append(options.AuthorizerOpts, docker.WithAuthClient(client))
 
 		rhosts := make([]docker.RegistryHost, len(hosts))
 		for i, host := range hosts {
 			// Allow setting for each host as well
 			explicitTLS := tlsConfigured
+
+			hostAuthOpts := authOpts
 
 			if host.caCerts != nil || host.clientPairs != nil || host.skipVerify != nil {
 				explicitTLS = true
@@ -237,11 +264,19 @@ func ConfigureHosts(ctx context.Context, options HostOptions) docker.RegistryHos
 				}
 
 				rhosts[i].Client = &c
-				rhosts[i].Authorizer = docker.NewDockerAuthorizer(append(authOpts, docker.WithAuthClient(&c))...)
+				// override the auth client set above
+				hostAuthOpts = append(hostAuthOpts, docker.WithAuthClient(&c))
 			} else {
 				rhosts[i].Client = client
-				rhosts[i].Authorizer = authorizer
 			}
+
+			authCreds := compositeAuthCredentials(
+				options.Credentials,
+				authCredentialFromHostConfig(hostNamespace, host),
+			)
+			hostAuthOpts = append(hostAuthOpts, docker.WithAuthCreds(authCreds))
+
+			rhosts[i].Authorizer = docker.NewDockerAuthorizer(hostAuthOpts...)
 
 			// When TLS has been configured for the operation or host and
 			// the protocol from the port number is ambiguous, use the
@@ -352,7 +387,15 @@ type hostFileConfig struct {
 	// API root endpoint.
 	OverridePath bool `toml:"override_path"`
 
-	// TODO: Credentials: helper? name? username? alternate domain? token?
+	// TODO: Credentials: helper? name? token?
+	// Username is the username to login the registry.
+	Username string `toml:"username"`
+	// Password is the password to login the registry.
+	Password string `toml:"password"`
+	// CredentialDomain is a domain alias for the mirror registry.
+	// If CredentialDomain matches the upstream host, the credentials specified
+	// in the host config will be used.
+	CredentialDomain string `toml:"credential_domain"`
 }
 
 func parseHostsFile(baseDir string, b []byte) ([]hostConfig, error) {
@@ -426,6 +469,9 @@ func parseHostConfig(server string, baseDir string, config hostFileConfig) (host
 		result.path = u.Path
 	}
 
+	result.username = config.Username
+	result.password = config.Password
+	result.credentialDomain = config.CredentialDomain
 	result.skipVerify = config.SkipVerify
 
 	if len(config.Capabilities) > 0 {
