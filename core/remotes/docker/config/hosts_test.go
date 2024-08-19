70877e6d91c51f08bacfcb850077290f13eb68bd
Jian Zeng <anonymousknight96@gmail.com>
2024-08-20 00:38:37 +0800

    test: add test cases
    Signed-off-by: Jian Zeng <anonymousknight96@gmail.com>

--- core/remotes/docker/config/hosts_test.go
+++ core/remotes/docker/config/hosts_test.go
@@ -20,8 +20,11 @@ import (
 	"bytes"
 	"context"
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"os"
 	"path/filepath"
 	"testing"
@@ -29,9 +32,148 @@ import (
 	"github.com/containerd/log/logtest"
 
 	"github.com/containerd/containerd/v2/core/remotes/docker"
+	"github.com/containerd/containerd/v2/core/remotes/docker/auth"
 )
 
 const allCaps = docker.HostCapabilityPull | docker.HostCapabilityResolve | docker.HostCapabilityPush
+
+func TestCredentialDomainOfHostConfig(t *testing.T) {
+	tmpDir := t.TempDir()
+	ctx, cancel := context.WithCancel(context.TODO())
+	defer cancel()
+
+	hostDir := filepath.Join(tmpDir, "docker.io")
+	err := os.MkdirAll(hostDir, 0o755)
+	if err != nil {
+		t.Fatal(err)
+	}
+	hostCfgPath := filepath.Join(hostDir, "hosts.toml")
+	err = os.WriteFile(hostCfgPath, []byte(`
+server = "https://registry-1.docker.io"
+
+[host."http://127.0.0.1:5000"]
+  capabilities = ["pull", "resolve"]
+  username = "foo"
+  password = "bar"
+  credential_domain = "docker.io"
+
+[host."http://127.0.0.1:5001"]
+  capabilities = ["pull", "resolve"]
+  username = "foo"
+  password = "bar"
+  credential_domain = "ghcr.io"
+`), 0o644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fakeAuthSvc := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		switch req.URL.Path {
+		case "/no-credential":
+			if req.Method != http.MethodGet {
+				t.Errorf("Unexpected method: %q", req.Method)
+				rw.WriteHeader(http.StatusMethodNotAllowed)
+				return
+			}
+			rw.Header().Set("Content-Type", "application/json")
+			_ = json.NewEncoder(rw).Encode(auth.FetchTokenResponse{
+				AccessToken: "public_token",
+			})
+
+		case "/validate-credential":
+			if req.Method != http.MethodPost {
+				t.Errorf("Unexpected method: %q", req.Method)
+				rw.WriteHeader(http.StatusMethodNotAllowed)
+				return
+			}
+			err := req.ParseForm()
+			if err != nil {
+				t.Errorf("Fail to parse form: %v", err)
+				rw.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			username := req.Form.Get("username")
+			passwd := req.Form.Get("password")
+			if username != "foo" || passwd != "bar" {
+				t.Errorf("Unexpected credentials. Got username=%q, password=%q", username, passwd)
+				rw.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			rw.Header().Set("Content-Type", "application/json")
+			_ = json.NewEncoder(rw).Encode(auth.OAuthTokenResponse{
+				AccessToken: "access_token",
+			})
+		}
+	}))
+	defer fakeAuthSvc.Close()
+
+	getRegistryHosts := ConfigureHosts(ctx, HostOptions{
+		Credentials: func(host string) (string, string, error) {
+			return "", "", nil
+		},
+		HostDir: HostDirFromRoot(tmpDir),
+	})
+
+	hosts, err := getRegistryHosts("docker.io")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(hosts) != 2+1 {
+		t.Fatalf("Unexpected hosts: %d", len(hosts))
+	}
+
+	dockerRegistryURL := &url.URL{
+		Scheme: "https",
+		Host:   "registry-1.docker.io",
+	}
+
+	testCases := map[string]struct {
+		authorizer          docker.Authorizer
+		realm               string
+		expectedAuthzHeader string
+	}{
+		"credential_domain matches upstream host": {
+			authorizer:          hosts[0].Authorizer,
+			realm:               fmt.Sprintf("%s/validate-credential", fakeAuthSvc.URL),
+			expectedAuthzHeader: "Bearer access_token",
+		},
+		"credential_domain mismatches upstream host": {
+			authorizer:          hosts[1].Authorizer,
+			realm:               fmt.Sprintf("%s/no-credential", fakeAuthSvc.URL),
+			expectedAuthzHeader: "Bearer public_token",
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			fakeAuthResp := &http.Response{
+				Request: &http.Request{
+					URL: dockerRegistryURL,
+				},
+				Header: http.Header{},
+			}
+			fakeAuthResp.Header.Add(
+				"WWW-Authenticate",
+				fmt.Sprintf(`Bearer realm="%s",service="registry.docker.io",scope="repository:library/nginx:pull"`, tc.realm),
+			)
+			err := tc.authorizer.AddResponses(ctx, []*http.Response{fakeAuthResp})
+			if err != nil {
+				t.Fatal(err)
+			}
+			req := &http.Request{
+				URL:    dockerRegistryURL,
+				Header: http.Header{},
+			}
+			err = tc.authorizer.Authorize(ctx, req)
+			if err != nil {
+				t.Fatal(err)
+			}
+			authzHeaderValue := req.Header.Get("Authorization")
+			if authzHeaderValue != tc.expectedAuthzHeader {
+				t.Errorf("Unexpected Authorization header. Expected: %q, got %q", tc.expectedAuthzHeader, authzHeaderValue)
+			}
+		})
+	}
+}
 
 func TestDefaultHosts(t *testing.T) {
 	ctx := logtest.WithT(context.Background(), t)
