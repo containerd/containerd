17181ed33e018a629deeb08889bef4cc3412c64e
Henry Wang <henwang@amazon.com>
2024-12-14 03:57:38 +0000

    fix master tty leak due to leaking init container object
    Signed-off-by: Henry Wang <henwang@amazon.com>

--- cmd/containerd-shim-runc-v2/task/service.go
+++ cmd/containerd-shim-runc-v2/task/service.go
@@ -369,6 +369,9 @@ func (s *service) Delete(ctx context.Context, r *taskAPI.DeleteRequest) (*taskAP
 			ExitStatus:  uint32(p.ExitStatus()),
 			ExitedAt:    protobuf.ToTimestamp(p.ExitedAt()),
 		})
+		s.lifecycleMu.Lock()
+		delete(s.containerInitExit, container)
+		s.lifecycleMu.Unlock()
 	}
 	return &taskAPI.DeleteResponse{
 		ExitStatus: uint32(p.ExitStatus()),
