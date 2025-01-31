7a14eae6b3abf0cb1f95e1cfa89f316fdb9b287c
Akhil Mohan <akhilerm@gmail.com>
2025-01-31 09:24:23 +0530

    initial implmentation of cpu usage metrics
    Signed-off-by: Akhil Mohan <akhilerm@gmail.com>

--- internal/cri/server/list_pod_sandbox_metrics.go
+++ internal/cri/server/list_pod_sandbox_metrics.go
@@ -18,7 +18,15 @@ package server
 
 import (
 	"context"
+	"fmt"
 
+	cg1 "github.com/containerd/cgroups/v3/cgroup1/stats"
+	cg2 "github.com/containerd/cgroups/v3/cgroup2/stats"
+	"github.com/containerd/containerd/api/services/tasks/v1"
+	"github.com/containerd/containerd/api/types"
+	"github.com/containerd/containerd/v2/pkg/protobuf"
+	"github.com/containerd/typeurl/v2"
+	"github.com/sirupsen/logrus"
 	runtime "k8s.io/cri-api/pkg/apis/runtime/v1"
 )
 
@@ -28,8 +36,17 @@ func (c *criService) ListPodSandboxMetrics(ctx context.Context, r *runtime.ListP
 
 	podMetrics := make([]*runtime.PodSandboxMetrics, 0)
 	for _, sandbox := range sandboxList {
-		c.containerStore.List()
+
 		containerMetrics := make([]*runtime.ContainerMetrics, 0)
+
+		containers := c.containerStore.List()
+		for _, container := range containers {
+			metrics, err := c.listContainerMetrics(sandbox.ID, container.ID)
+			if err != nil {
+				logrus.Debug("failed to fetch metrics %v", err)
+			}
+			containerMetrics = append(containerMetrics, metrics)
+		}
 
 		podMetrics = append(podMetrics, &runtime.PodSandboxMetrics{
 			PodSandboxId:     sandbox.ID,
@@ -42,7 +59,60 @@ func (c *criService) ListPodSandboxMetrics(ctx context.Context, r *runtime.ListP
 	}, nil
 }
 
-// gives the metrics for a given container in a sandbox
-func (c *criService) listContainerMetrics(sandboxID string, containerID string) *runtime.ContainerMetrics {
+type containerMetrics struct {
+	metrics *runtime.ContainerMetrics
+}
 
+// gives the metrics for a given container in a sandbox
+func (c *criService) listContainerMetrics(ctx context.Context, sandboxID string, containerID string) (*runtime.ContainerMetrics, error) {
+	request := &tasks.MetricsRequest{Filters: []string{"id==" + containerID}}
+	resp, err := c.client.TaskService().Metrics(ctx, request)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch metrics for task: %w", err)
+	}
+	if len(resp.Metrics) != 1 {
+		return nil, fmt.Errorf("unexpected metrics response: %+v", resp.Metrics)
+	}
+	metric, err := c.toContainerMetrics(ctx, containerID, resp.Metrics[0])
+	if err != nil {
+		return nil, err
+	}
+	return metric.metrics, nil
+
+}
+
+func (c *criService) toContainerMetrics(ctx context.Context, containerdID string, metrics *types.Metric) (containerMetrics, error) {
+	var cm runtime.ContainerMetrics
+	var metric runtime.Metric
+	//var pids uint64
+	if metrics != nil {
+		var data interface{}
+		switch {
+		case typeurl.Is(metrics.Data, (*cg1.Metrics)(nil)):
+			data = &cg1.Metrics{}
+			if err := typeurl.UnmarshalTo(metrics.Data, data); err != nil {
+				return containerMetrics{}, fmt.Errorf("failed to extract container metrics: %w", err)
+			}
+			//pids = data.(*cg1.Metrics).GetPids().GetCurrent()
+		case typeurl.Is(metrics.Data, (*cg2.Metrics)(nil)):
+			data = &cg2.Metrics{}
+			if err := typeurl.UnmarshalTo(metrics.Data, data); err != nil {
+				return containerMetrics{}, fmt.Errorf("failed to extract container metrics: %w", err)
+			}
+			//pids = data.(*cg2.Metrics).GetPids().GetCurrent()
+		default:
+			return containerMetrics{}, fmt.Errorf("cannot convert metric data to cgroups.Metrics")
+		}
+
+		cpuStats, err := c.cpuContainerStats(containerdID, false /* not a sandbox */, data, protobuf.FromTimestamp(metrics.Timestamp))
+		if err != nil {
+			return containerMetrics{}, fmt.Errorf("failed to obtain cpu stats: %w", err)
+		}
+		metric.MetricType = runtime.MetricType_GAUGE
+		metric.Name = "container_cpu_usage_seconds_total"
+		metric.Value = cpuStats.UsageCoreNanoSeconds
+		cm.Metrics = append(cm.Metrics, &metric)
+
+	}
+	return containerMetrics{metrics: &cm}, nil
 }
