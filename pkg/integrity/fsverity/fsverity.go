14e9141464f3bc3e2ce6f184e5a933cb3516974d
James Jenkins <James.Jenkins@ibm.com>
2025-02-21 15:40:15 -0500

    Add copyright header to new files
    Signed-off-by: James Jenkins <James.Jenkins@ibm.com>

--- pkg/integrity/fsverity/fsverity.go
+++ pkg/integrity/fsverity/fsverity.go
@@ -1,3 +1,19 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package fsverity
 
 import (
@@ -90,7 +106,7 @@ func (v validator) IsValid(blob string) (bool, error) {
 	var expectedDigest string
 	digest := filepath.Base(blob)
 	integrityFile := filepath.Join(v.integrityStorePath, digest)
-	ifd, err := os.Open(integrityFile) // TODO: validate the signed digest next?
+	ifd, err := os.Open(integrityFile)
 	if err != nil {
 		return false, fmt.Errorf("could not read expected integrity value of %s", blob)
 	}
