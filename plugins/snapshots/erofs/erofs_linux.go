containerd/containerd

commit b477cf8e97b6facd183bba964631a36ef7a3d32b

erofs-snapshotter: protect layer blobs with FS_IMMUTABLE_FL
As documented in ioctl_iflags(2):
```
 FS_IMMUTABLE_FL
  The file is immutable: no changes are permitted to the file contents
  or metadata (permissions, timestamps, ownership, link count, and so
  on).  (This restriction applies even to the superuser.)
```

For example, any user cannot delete/move layer blobs when
FS_IMMUTABLE_FL is set:
``` sh
 # cd /var/lib/containerd/io.containerd.snapshotter.v1.erofs/snapshots/4
 # mv layer{,1}.erofs
 mv: cannot move 'layer.erofs' to 'layer1.erofs': Operation not permitted
 # rm layer.erofs
 rm: cannot remove 'layer.erofs': Operation not permitted
```

Note that it's a best-effort approach for data loss prevention.  IOWs,
just warn out if FS_IMMUTABLE_FL cannot be set anyway (e.g., due to lack
of support in the underlying filesystem.)

Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

plugins/snapshots/erofs/erofs_linux.go
@@ -355,6 +355,31 @@ func (s *snapshotter) View(ctx context.Context, key, parent string, opts ...snap
 	return s.createSnapshot(ctx, snapshots.KindView, key, parent, opts)
 }
 
+func setImmutable(path string, enable bool) error {
+	//nolint:revive	// silence "don't use ALL_CAPS in Go names; use CamelCase"
+	const (
+		FS_IMMUTABLE_FL = 0x10
+	)
+	f, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("failed to open: %w", err)
+	}
+	defer f.Close()
+
+	oldattr, err := unix.IoctlGetInt(int(f.Fd()), unix.FS_IOC_GETFLAGS)
+	if err != nil {
+		return fmt.Errorf("error getting inode flags: %w", err)
+	}
+	newattr := oldattr | FS_IMMUTABLE_FL
+	if !enable {
+		newattr ^= FS_IMMUTABLE_FL
+	}
+	if newattr == oldattr {
+		return nil
+	}
+	return unix.IoctlSetPointerInt(int(f.Fd()), unix.FS_IOC_SETFLAGS, newattr)
+}
+
 func (s *snapshotter) Commit(ctx context.Context, name, key string, opts ...snapshots.Opt) error {
 	var layerBlob, upperDir string
 
@@ -403,7 +428,10 @@ func (s *snapshotter) Commit(ctx context.Context, name, key string, opts ...snap
 				return fmt.Errorf("failed to enable fsverity: %w", err)
 			}
 		}
-
+		// Set IMMUTABLE_FL on the EROFS layer to avoid artificial data loss
+		if err := setImmutable(layerBlob, true); err != nil {
+			log.G(ctx).WithError(err).Warnf("failed to set IMMUTABLE_FL for %s", layerBlob)
+		}
 		return nil
 	})
 
@@ -506,6 +534,11 @@ func (s *snapshotter) Remove(ctx context.Context, key string) (err error) {
 		removals, err = s.getCleanupDirectories(ctx)
 		if err != nil {
 			return fmt.Errorf("unable to get directories for removal: %w", err)
+		}
+		// Clear IMMUTABLE_FL before removal, since this flag avoids it.
+		err = setImmutable(s.layerBlobPath(id), false)
+		if err != nil {
+			return fmt.Errorf("failed to clear IMMUTABLE_FL: %w", err)
 		}
 		return nil
 	})

commit 3a5de731c587342ccc8691acd5d4ae2154b9511c

erofs-snapshotter: clear IMMUTABLE_FL only for committed snapshots
Otherwise, the following error avoids snapshot GC:
level=warning msg="snapshot garbage collection failed" error="failed
to clear IMMUTABLE_FL: failed to open: open /var/lib/containerd/io.
containerd.snapshotter.v1.erofs/snapshots/2/layer.erofs: no such file
or directory

Fixes: b477cf8e97b6 ("erofs-snapshotter: protect layer blobs with FS_IMMUTABLE_FL")
Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

plugins/snapshots/erofs/erofs_linux.go
@@ -528,7 +528,9 @@ func (s *snapshotter) Remove(ctx context.Context, key string) (err error) {
 		}
 	}()
 	return s.ms.WithTransaction(ctx, true, func(ctx context.Context) error {
-		id, _, err = storage.Remove(ctx, key)
+		var k snapshots.Kind
+
+		id, k, err = storage.Remove(ctx, key)
 		if err != nil {
 			return fmt.Errorf("failed to remove snapshot %s: %w", key, err)
 		}
@@ -537,10 +539,13 @@ func (s *snapshotter) Remove(ctx context.Context, key string) (err error) {
 		if err != nil {
 			return fmt.Errorf("unable to get directories for removal: %w", err)
 		}
-		// Clear IMMUTABLE_FL before removal, since this flag avoids it.
-		err = setImmutable(s.layerBlobPath(id), false)
-		if err != nil {
-			return fmt.Errorf("failed to clear IMMUTABLE_FL: %w", err)
+		// The layer blob is only persisted for committed snapshots.
+		if k == snapshots.KindCommitted {
+			// Clear IMMUTABLE_FL before removal, since this flag avoids it.
+			err = setImmutable(s.layerBlobPath(id), false)
+			if err != nil {
+				return fmt.Errorf("failed to clear IMMUTABLE_FL: %w", err)
+			}
 		}
 		return nil
 	})