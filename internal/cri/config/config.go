edd1cc50d5f3c474fe6f09927afbe9be4c7c10da
Samuel Karp <samuelkarp@google.com>
2025-03-06 16:30:45 -0800

    docs: include note about unprivileged sysctls
    We changed the default setting for `enable_unprivileged_ports` and
    `enable_unprivileged_icmp` in the CRI plugin in
    https://github.com/containerd/containerd/pull/9348, but missed including
    this change in the release notes.
    
    Signed-off-by: Samuel Karp <samuelkarp@google.com>

--- internal/cri/config/config.go
+++ internal/cri/config/config.go
@@ -381,13 +381,10 @@ type RuntimeConfig struct {
 	// EnableUnprivilegedPorts configures net.ipv4.ip_unprivileged_port_start=0
 	// for all containers which are not using host network
 	// and if it is not overwritten by PodSandboxConfig
-	// Note that currently default is set to disabled but target change it in future, see:
-	//   https://github.com/kubernetes/kubernetes/issues/102612
 	EnableUnprivilegedPorts bool `toml:"enable_unprivileged_ports" json:"enableUnprivilegedPorts"`
 	// EnableUnprivilegedICMP configures net.ipv4.ping_group_range="0 2147483647"
 	// for all containers which are not using host network, are not running in user namespace
 	// and if it is not overwritten by PodSandboxConfig
-	// Note that currently default is set to disabled but target change it in future together with EnableUnprivilegedPorts
 	EnableUnprivilegedICMP bool `toml:"enable_unprivileged_icmp" json:"enableUnprivilegedICMP"`
 	// EnableCDI indicates to enable injection of the Container Device Interface Specifications
 	// into the OCI config
