b10ef495704def21e779d8de8c217039218487c0
ningmingxiao <ning.mingxiao@zte.com.cn>
2025-04-07 10:39:25 +0800

    client:support to delete create status task
    Signed-off-by: ningmingxiao <ning.mingxiao@zte.com.cn>

--- client/task.go
+++ client/task.go
@@ -385,7 +385,12 @@ func (t *task) Delete(ctx context.Context, opts ...ProcessDeleteOpts) (*ExitStat
 			// https://github.com/containerd/containerd/issues/7357
 			break
 		}
-		fallthrough
+		_, err := t.client.TaskService().Kill(ctx, &tasks.KillRequest{
+			ContainerID: t.id,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to kill task %s:%w", t.id, err)
+		}
 	default:
 		return nil, fmt.Errorf("task must be stopped before deletion: %s: %w", status.Status, errdefs.ErrFailedPrecondition)
 	}
