4ba3d82ca270937a428d4b6c006bd7e9f8261743
Akhil Mohan <akhilerm@gmail.com>
2025-04-16 22:02:06 +0530

    add option to skip tests in critest
    Signed-off-by: Akhil Mohan <akhilerm@gmail.com>

--- script/critest.sh
+++ script/critest.sh
@@ -70,6 +70,11 @@ if [ ! -z "$CGROUP_DRIVER" ] && [ "$CGROUP_DRIVER" = "systemd" ];then
 EOF
 fi
 
+GINKGO_SKIP_TEST=()
+if [ ! -z "$SKIP_TEST" ]; then
+  GINKGO_SKIP_TEST+=("--ginkgo.skip" "$SKIP_TEST")
+fi
+
 ls /etc/cni/net.d
 
 /usr/local/bin/containerd \
@@ -85,4 +90,4 @@ do
     crictl --runtime-endpoint ${BDIR}/c.sock info && break || sleep 1
 done
 
-critest --report-dir "$report_dir" --runtime-endpoint=unix:///${BDIR}/c.sock --parallel=8 "${EXTRA_CRITEST_OPTIONS:-""}"
+critest --report-dir "$report_dir" --runtime-endpoint=unix:///${BDIR}/c.sock --parallel=8 "${GINKGO_SKIP_TEST[@]}" "${EXTRA_CRITEST_OPTIONS:-""}"
