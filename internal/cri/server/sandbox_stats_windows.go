d93d18c857e0fd31feed4b3b8084d36d90f8beb7
yashsingh74 <yashsingh1774@gmail.com>
2025-04-19 11:05:13 +0530

    fix: QF1001: could apply De Morgan's law (staticcheck)
    Signed-off-by: yashsingh74 <yashsingh1774@gmail.com>

--- internal/cri/server/sandbox_stats_windows.go
+++ internal/cri/server/sandbox_stats_windows.go
@@ -106,6 +106,7 @@ func convertMetricsToWindowsStats(metrics []*types.Metric, sandbox sandboxstore.
 		// In the case of HostProcess sandbox container we will use the nil value for the statsmap which is used later
 		// otherwise return an error since we should have gotten stats
 		containerStats, ok := containerStatsData.(*wstats.Statistics)
+		//nolint:staticcheck // QF1001: could apply De Morgan's law (staticcheck)
 		if !ok && !(isHostProcess && sandbox.ID == stat.ID) {
 			return nil, fmt.Errorf("failed to extract metrics for container with id %s: %w", stat.ID, err)
 		}
