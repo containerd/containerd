d235465d68ea7ccd928d29b58494614c576f8ee1
Kirtana Ashok <kiashok@microsoft.com>
2025-04-29 09:05:02 -0700

    Plumb windows CPU affinity values to runtime spec
    Commit passes the CPU affinity values namely
    cpu mask and group to runtime spec from container
    config.
    
    Signed-off-by: Kirtana Ashok <kiashok@microsoft.com>

--- internal/cri/server/container_create.go
+++ internal/cri/server/container_create.go
@@ -1028,6 +1028,10 @@ func (c *criService) buildWindowsSpec(
 		annotations.DefaultCRIAnnotations(sandboxID, containerName, imageName, sandboxConfig, false)...,
 	)
 
+	if config.Windows != nil {
+		specOpts = append(specOpts, customopts.WithWindowsAffinityCPUs(config.Windows))
+	}
+
 	return specOpts, nil
 }
 
