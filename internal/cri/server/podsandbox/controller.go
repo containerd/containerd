38cbc41647b771a118ce8451e4c32c2bf1edec14
ningmingxiao <ning.mingxiao@zte.com.cn>
2025-06-10 11:55:25 +0800

    [bugfix]:add lock to prevent race between stopSandboxContainer and handleSandboxTaskExit
    Signed-off-by: ningmingxiao <ning.mingxiao@zte.com.cn>

--- internal/cri/server/podsandbox/controller.go
+++ internal/cri/server/podsandbox/controller.go
@@ -36,6 +36,7 @@ import (
 	"github.com/containerd/containerd/v2/internal/cri/server/podsandbox/types"
 	imagestore "github.com/containerd/containerd/v2/internal/cri/store/image"
 	ctrdutil "github.com/containerd/containerd/v2/internal/cri/util"
+	"github.com/containerd/containerd/v2/internal/kmutex"
 	"github.com/containerd/containerd/v2/pkg/oci"
 	osinterface "github.com/containerd/containerd/v2/pkg/os"
 	"github.com/containerd/containerd/v2/pkg/protobuf"
@@ -86,6 +87,7 @@ func init() {
 				runtimeService: runtimeService,
 				imageService:   criImagePlugin.(ImageService),
 				store:          NewStore(),
+				locker:         kmutex.New(),
 			}
 
 			eventMonitor := events.NewEventMonitor(&podSandboxEventHandler{
@@ -130,7 +132,8 @@ type Controller struct {
 	// actually we only use it's backoff mechanism to make sure pause container is cleaned up.
 	eventMonitor *events.EventMonitor
 
-	store *Store
+	store  *Store
+	locker kmutex.KeyedLocker
 }
 
 var _ sandbox.Controller = (*Controller)(nil)
@@ -177,7 +180,7 @@ func (c *Controller) waitSandboxExit(ctx context.Context, p *types.PodSandbox, e
 		dctx, dcancel := context.WithTimeout(dctx, handleEventTimeout)
 		defer dcancel()
 		event := &eventtypes.TaskExit{ExitStatus: exitStatus, ExitedAt: protobuf.ToTimestamp(exitedAt)}
-		if err := handleSandboxTaskExit(dctx, p, event); err != nil {
+		if err := c.handleSandboxTaskExit(dctx, p, event); err != nil {
 			c.eventMonitor.Backoff(p.ID, event)
 		}
 		return nil
@@ -187,8 +190,10 @@ func (c *Controller) waitSandboxExit(ctx context.Context, p *types.PodSandbox, e
 }
 
 // handleSandboxTaskExit handles TaskExit event for sandbox.
-func handleSandboxTaskExit(ctx context.Context, sb *types.PodSandbox, e *eventtypes.TaskExit) error {
+func (c *Controller) handleSandboxTaskExit(ctx context.Context, sb *types.PodSandbox, e *eventtypes.TaskExit) error {
 	// No stream attached to sandbox container.
+	c.locker.Lock(context.Background(), sb.ID)
+	defer c.locker.Unlock(sb.ID)
 	task, err := sb.Container.Task(ctx, nil)
 	if err != nil {
 		if !errdefs.IsNotFound(err) {
