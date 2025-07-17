280806186e2cad01997441264c65ff03bff0ead8
蔡秀吉 <84045975+thc1006@users.noreply.github.com>
2025-07-17 15:28:36 +0800

    Create retry.go
    Signed-off-by: 蔡秀吉 <84045975+thc1006@users.noreply.github.com>

---
+++ pkg/fstest/retry.go
@@ -0,0 +1,40 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package fstest
+
+import (
+	"context"
+	"time"
+)
+
+// Retry with a linear backoff.
+func Retry(ctx context.Context, maxAttempts int, delay time.Duration, fn func() error) error {
+	var err error
+	for i := 0; i < maxAttempts; i++ {
+		err = fn()
+		if err == nil {
+			return nil
+		}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-time.After(delay):
+		}
+	}
+	return err
+}
