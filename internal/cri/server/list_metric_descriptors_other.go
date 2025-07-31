63eca8fe9f9e56dace76326ac88beb7d43702cbc
Akhil Mohan <akhilerm@gmail.com>
2025-07-31 08:44:36 +0530

    implement CRI ListMetricDescriptors
    Signed-off-by: Akhil Mohan <akhilerm@gmail.com>

--- internal/cri/server/list_metric_descriptors_other.go
+++ internal/cri/server/list_metric_descriptors_other.go
@@ -1,3 +1,5 @@
+//go:build !linux
+
 /*
    Copyright The containerd Authors.
 
@@ -16,6 +18,14 @@
 
 package server
 
-const (
-	CPUUsageMetrics = "cpu"
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	runtime "k8s.io/cri-api/pkg/apis/runtime/v1"
 )
+
+func (c *criService) ListMetricDescriptors(context.Context, *runtime.ListMetricDescriptorsRequest) (*runtime.ListMetricDescriptorsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListMetricDescriptors not implemented on this platform")
+}
