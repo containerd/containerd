da3dc1ef6c3d568f33c43cb22bc78b13963584ff
Rodrigo Campos <rodrigoca@microsoft.com>
2025-08-07 15:12:30 -0300

    core/mount: Retry unmounting idmapped directories
    When a lot of pods are created all at the same time, the umount fails
    with EBUSY sometimes. This causes some mounts to be leaked. A way to
    repro this issue is here:
    	https://github.com/containerd/containerd/issues/12139#issuecomment-3165305904
    
    While using lsof/fuser it was not possible to get the culprits on time
    (the mount is busy for a few ms), @fuwei has found what is causing the
    mount to be busy:
    	https://github.com/containerd/containerd/issues/12139#issuecomment-3184544534
    
    When we fork in `GetUsernsFD()`, if a call to prepareIDMappedOverlay()
    is ongoing and has an open fd to the path to idmap, then the unmount
    callback will fail as the forked process still has an open fd to it.
    
    Let's handle the idmap unmounts in the same way other temp mounts are
    done, using the Unmount() helper, that retries the unmount.
    
    This retry fixes it 100% of the times on my system and in the systems
    of @halaney that reported the issue too.
    
    This was originally fixed by #10721 by using a detached mount, but the
    mount was switched to non-detached again in #10955 and we started to
    leak mounts. As @fuwei mentioned, using a detached mount is quite
    invisible for the admin and, therefore, a retry is a better alternative.
    
    It seems these umounts are a great candidate for #11303, which will
    manage the life-cycle of mounts and can handle these retries whenever
    needed.
    
    [1]: To do it, I just run as root "unshare -m", that creates a mntns
    with private propagation, and then run the containerd daemon.
    
    Signed-off-by: Rodrigo Campos <rodrigoca@microsoft.com>

--- core/mount/mount_linux.go
+++ core/mount/mount_linux.go
@@ -266,8 +266,12 @@ func doPrepareIDMappedOverlay(tmpDir string, lowerDirs []string, usernsFd int) (
 		return nil, nil, err
 	}
 	cleanMount := func() {
-		if err := unix.Unmount(tempRemountsLocation, 0); err != nil {
-			log.L.WithError(err).Warnf("failed to unmount idmapped directory %s", tempRemountsLocation)
+		// Use the Unmount helper that does retries because there can be easily an open fd
+		// to the idmapped directory and when containerd forks to create a userns fd (maybe
+		// for another container), it will make the mount busy for a few ms.
+		err := Unmount(tempRemountsLocation, 0)
+		if err != nil {
+			log.L.WithError(err).Warnf("failed to unmount idmapped directory %s: %v", tempRemountsLocation, err)
 		}
 	}
 	defer func() {
