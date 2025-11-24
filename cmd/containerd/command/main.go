adf626dae92a6abff0e5e3bb90d4565394d7a2ef
Derek McGowan <derek@mcg.dev>
2025-11-23 21:33:32 -0800

    Add metrics server plugin
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- cmd/containerd/command/main.go
+++ cmd/containerd/command/main.go
@@ -20,7 +20,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"net"
 	"os"
 	"os/signal"
 	"path/filepath"
@@ -237,17 +236,6 @@ can be used and modified as necessary as a custom configuration.`
 			return err
 		}
 
-		// TODO: Move to server.Start(ctx) error
-		if config.Metrics.Address != "" {
-			l, err := net.Listen("tcp", config.Metrics.Address)
-			if err != nil {
-				return fmt.Errorf("failed to get listener for metrics endpoint: %w", err)
-			}
-			serve(ctx, l, server.ServeMetrics)
-		}
-
-		// end s.Start
-
 		readyC := make(chan struct{})
 		go func() {
 			server.Wait()
@@ -266,17 +254,6 @@ can be used and modified as necessary as a custom configuration.`
 		return nil
 	}
 	return app
-}
-
-func serve(ctx context.Context, l net.Listener, serveFunc func(net.Listener) error) {
-	path := l.Addr().String()
-	log.G(ctx).WithField("address", path).Info("serving...")
-	go func() {
-		defer l.Close()
-		if err := serveFunc(l); err != nil {
-			log.G(ctx).WithError(err).WithField("address", path).Fatal("serve failure")
-		}
-	}()
 }
 
 func applyFlags(cliContext *cli.Context, config *srvconfig.Config) ([]server.UpdateConfig, error) {
