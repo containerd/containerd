1fe488ff96a605249c93ccd45b106d3dceca2244
ningmingxiao <ning.mingxiao@zte.com.cn>
2026-03-02 23:54:40 +0800

    client: stream containers serially Co-authored-by: Sebastiaan van Stijn <github@gone.nl>
    Signed-off-by: ningmingxiao <ning.mingxiao@zte.com.cn>

--- integration/client/benchmark_test.go
+++ integration/client/benchmark_test.go
@@ -17,7 +17,10 @@
 package client
 
 import (
+	"context"
 	"fmt"
+	"iter"
+	"strings"
 	"testing"
 
 	. "github.com/containerd/containerd/v2/client"
@@ -66,6 +69,123 @@ func BenchmarkContainerCreate(b *testing.B) {
 		containers = append(containers, container)
 	}
 	b.StopTimer()
+}
+
+func BenchmarkContainerGet(b *testing.B) {
+	client, err := newClient(b, address)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer client.Close()
+
+	ctx, cancel := testContext(b)
+	defer cancel()
+	image, err := client.GetImage(ctx, testImage)
+	if err != nil {
+		b.Error(err)
+		return
+	}
+	var containers []Container
+	defer func() {
+		for _, c := range containers {
+			if err := c.Delete(ctx, WithSnapshotCleanup); err != nil {
+				b.Error(err)
+			}
+		}
+	}()
+
+	var envs []string
+	// Simulate injecting a large number of Kubernetes service environment variables.
+	// see  https://github.com/kubernetes/kubernetes/issues/121787
+	// so that it will take up more memory when fetching the container list.
+	envs = append(envs, generateK8sServiceEnvVarsList()...)
+	for i := 0; i < 500; i++ {
+		id := fmt.Sprintf("%s-%d", b.Name(), i)
+		container, err := client.NewContainer(ctx, id, WithNewSnapshot(id, image), WithNewSpec(oci.WithImageConfig(image), oci.WithEnv(envs), withExitStatus(7)))
+		if err != nil {
+			b.Error(err)
+			return
+		}
+		b.Logf("create container %s", container.ID())
+		containers = append(containers, container)
+	}
+	b.Run("use client.ContainersIter", benchmarkGetContainers(ctx, true, client))
+	b.Run("use client.Containers", benchmarkGetContainers(ctx, false, client))
+}
+
+func benchmarkGetContainers(ctx context.Context, useIter bool, client *Client) func(b *testing.B) {
+	return func(b *testing.B) {
+		b.ResetTimer()
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			var filter []string
+			var containers iter.Seq2[Container, error]
+			var err error
+			if useIter {
+				containers, err = client.ContainersIter(ctx, filter...)
+				if err != nil {
+					b.Fatal(err)
+				}
+			} else {
+				ctrs, err := client.Containers(ctx, filter...)
+				if err != nil {
+					b.Fatal(err)
+				}
+				containers = sliceIter(ctrs)
+			}
+			for con, err := range containers {
+				if err != nil {
+					b.Fatal(err)
+				}
+				_, err := con.Info(ctx, WithoutRefreshedMetadata)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		}
+	}
+}
+
+func sliceIter(ctrs []Container) iter.Seq2[Container, error] {
+	return func(yield func(Container, error) bool) {
+		for _, c := range ctrs {
+			if !yield(c, nil) {
+				return
+			}
+		}
+	}
+}
+
+const (
+	fixedIP       = "172.20.200.100"
+	fixedPort     = 8080
+	servicePrefix = "long-k8s-service-for-test-env-var-"
+	portTemplate  = "tcp://%s:%d"
+)
+
+func generateK8sServiceEnvVarsList() []string {
+	var envVarsList []string
+	for i := 1; i <= 4000; i++ {
+		serviceName := fmt.Sprintf("%s%03d-extra-long-suffix-to-increase-length-abcdefghijklmnop",
+			servicePrefix, i)
+		if len(serviceName) > 50 {
+			serviceName = serviceName[:50]
+		}
+		serviceName = strings.TrimSuffix(serviceName, "-")
+		envPrefix := strings.ToUpper(strings.ReplaceAll(serviceName, "-", "_"))
+		portStr := fmt.Sprintf("%d", fixedPort)
+		fullPort := fmt.Sprintf(portTemplate, fixedIP, fixedPort)
+		envVarsList = append(envVarsList,
+			fmt.Sprintf("%s_SERVICE_HOST=%s", envPrefix, fixedIP),
+			fmt.Sprintf("%s_SERVICE_PORT=%s", envPrefix, portStr),
+			fmt.Sprintf("%s_PORT=%s", envPrefix, fullPort),
+			fmt.Sprintf("%s_PORT_%d_TCP=%s", envPrefix, fixedPort, fullPort),
+			fmt.Sprintf("%s_PORT_%d_TCP_PROTO=tcp", envPrefix, fixedPort),
+			fmt.Sprintf("%s_PORT_%d_TCP_PORT=%s", envPrefix, fixedPort, portStr),
+			fmt.Sprintf("%s_PORT_%d_TCP_ADDR=%s", envPrefix, fixedPort, fixedIP),
+		)
+	}
+	return envVarsList
 }
 
 func BenchmarkContainerStart(b *testing.B) {
