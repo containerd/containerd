0ba7106b3a2f4a0d8a8ff9a4c8bb7316e6c01f27
ningmingxiao <ning.mingxiao@zte.com.cn>
2026-03-02 23:54:40 +0800

    client: stream containers serially Co-authored-by: Sebastiaan van Stijn <github@gone.nl>
    Signed-off-by: ningmingxiao <ning.mingxiao@zte.com.cn>

--- integration/client/benchmark_test.go
+++ integration/client/benchmark_test.go
@@ -17,7 +17,9 @@
 package client
 
 import (
+	"context"
 	"fmt"
+	"iter"
 	"testing"
 
 	. "github.com/containerd/containerd/v2/client"
@@ -66,6 +68,92 @@ func BenchmarkContainerCreate(b *testing.B) {
 		containers = append(containers, container)
 	}
 	b.StopTimer()
+}
+
+func BenchmarkContainerGet(b *testing.B) {
+	client, err := newClient(b, address)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer client.Close()
+
+	ctx, cancel := testContext(b)
+	defer cancel()
+	image, err := client.GetImage(ctx, testImage)
+	if err != nil {
+		b.Error(err)
+		return
+	}
+	var containers []Container
+	defer func() {
+		for _, c := range containers {
+			if err := c.Delete(ctx, WithSnapshotCleanup); err != nil {
+				b.Error(err)
+			}
+		}
+	}()
+
+	var envs []string
+	// Randomly generate a large list of environment variables to create a container,
+	// so that it will take up more memory when fetching the container list.
+	for key := range 4000 {
+		envs = append(envs, fmt.Sprintf("SFTPV6_CFBFC1C7_C8A8_00C_94AC_4EFD3CE927D2_PORT_0001_TCP_PORT%d=value_%d", key, key))
+	}
+	for i := 0; i < 500; i++ {
+		id := fmt.Sprintf("%s-%d", b.Name(), i)
+		container, err := client.NewContainer(ctx, id, WithNewSnapshot(id, image), WithNewSpec(oci.WithImageConfig(image), oci.WithEnv(envs), withExitStatus(7)))
+		if err != nil {
+			b.Error(err)
+			return
+		}
+		b.Logf("create container %s", container.ID())
+		containers = append(containers, container)
+	}
+	b.Run("use client.ContainersIter", benchmarkGetContainers(ctx, true, client))
+	b.Run("use client.Containers", benchmarkGetContainers(ctx, false, client))
+}
+
+func benchmarkGetContainers(ctx context.Context, useIter bool, client *Client) func(b *testing.B) {
+	return func(b *testing.B) {
+		b.ResetTimer()
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			var filter []string
+			var containers iter.Seq2[Container, error]
+			var err error
+			if useIter {
+				containers, err = client.ContainersIter(ctx, filter...)
+				if err != nil {
+					b.Fatal(err)
+				}
+			} else {
+				ctrs, err := client.Containers(ctx, filter...)
+				if err != nil {
+					b.Fatal(err)
+				}
+				containers = sliceIter(ctrs)
+			}
+			for con, err := range containers {
+				if err != nil {
+					b.Fatal(err)
+				}
+				_, err := con.Info(ctx, WithoutRefreshedMetadata)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		}
+	}
+}
+
+func sliceIter(ctrs []Container) iter.Seq2[Container, error] {
+	return func(yield func(Container, error) bool) {
+		for _, c := range ctrs {
+			if !yield(c, nil) {
+				return
+			}
+		}
+	}
 }
 
 func BenchmarkContainerStart(b *testing.B) {
