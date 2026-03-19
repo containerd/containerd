64ea9da30f6acbab0ec636d96ed1fae334db095f
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2026-03-19 10:05:21 -0700

    Use enums instead of strings for capabilities and log level
    Signed-off-by: Maksym Pavlenko <pavlenko.maksym@gmail.com>

--- pkg/shim/deprecated.go
+++ pkg/shim/deprecated.go
@@ -71,7 +71,7 @@ func (m *managerShim) Start(ctx context.Context, params *bootapi.BootstrapParams
 	opts := StartOpts{
 		Address:      params.ContainerdGrpcAddress,
 		TTRPCAddress: params.ContainerdTtrpcAddress,
-		Debug:        params.LogLevel == "debug" || params.LogLevel == "trace",
+		Debug:        params.LogLevel == bootapi.LogLevel_LOG_LEVEL_DEBUG || params.LogLevel == bootapi.LogLevel_LOG_LEVEL_TRACE,
 	}
 
 	bp, err := m.manager.Start(ctx, params.InstanceID, opts)
