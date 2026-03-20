7acb6f5ba785b728e1fedc51d26e2ffc1ca496aa
ningmingxiao <ning.mingxiao@zte.com.cn>
2026-03-20 10:37:12 +0800

    cri: support to disable statsCollector
    Signed-off-by: ningmingxiao <ning.mingxiao@zte.com.cn>

--- internal/cri/config/config.go
+++ internal/cri/config/config.go
@@ -441,6 +441,8 @@ type RuntimeConfig struct {
 	//   https://golang.org/pkg/time/#ParseDuration
 	// Default: "2m"
 	StatsRetentionPeriod string `toml:"stats_retention_period" json:"statsRetentionPeriod"`
+	//  EnableStatsCollector indicates to enable the StatsCollector support.
+	EnableStatsCollector bool `toml:"enable_statscollector" json:"enableStatsCollector"`
 }
 
 // X509KeyPairStreaming contains the x509 configuration for streaming
