121f3a21e438cd8c18c6d76cbab1514ee2a8d8d2
Derek McGowan <derek@mcg.dev>
2026-04-02 22:02:36 -0700

    Add transfer types for container filesystem copy
    Add support for transfering data to and from a container filesystem.
    
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- vendor/github.com/containerd/containerd/api/types/transfer/streaming.pb.go
+++ vendor/github.com/containerd/containerd/api/types/transfer/streaming.pb.go
@@ -129,6 +129,122 @@ func (x *WindowUpdate) GetUpdate() int32 {
 	return 0
 }
 
+// ReadStream carries data from the client to the server (import
+// direction). The client sends data through the stream and the
+// server reads it.
+type ReadStream struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Stream    string `protobuf:"bytes,1,opt,name=stream,proto3" json:"stream,omitempty"`
+	MediaType string `protobuf:"bytes,2,opt,name=media_type,json=mediaType,proto3" json:"media_type,omitempty"`
+}
+
+func (x *ReadStream) Reset() {
+	*x = ReadStream{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_types_transfer_streaming_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ReadStream) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ReadStream) ProtoMessage() {}
+
+func (x *ReadStream) ProtoReflect() protoreflect.Message {
+	mi := &file_types_transfer_streaming_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ReadStream.ProtoReflect.Descriptor instead.
+func (*ReadStream) Descriptor() ([]byte, []int) {
+	return file_types_transfer_streaming_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *ReadStream) GetStream() string {
+	if x != nil {
+		return x.Stream
+	}
+	return ""
+}
+
+func (x *ReadStream) GetMediaType() string {
+	if x != nil {
+		return x.MediaType
+	}
+	return ""
+}
+
+// WriteStream carries data from the server to the client (export
+// direction). The server writes data into the stream and the
+// client receives it.
+type WriteStream struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Stream    string `protobuf:"bytes,1,opt,name=stream,proto3" json:"stream,omitempty"`
+	MediaType string `protobuf:"bytes,2,opt,name=media_type,json=mediaType,proto3" json:"media_type,omitempty"`
+}
+
+func (x *WriteStream) Reset() {
+	*x = WriteStream{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_types_transfer_streaming_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *WriteStream) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*WriteStream) ProtoMessage() {}
+
+func (x *WriteStream) ProtoReflect() protoreflect.Message {
+	mi := &file_types_transfer_streaming_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use WriteStream.ProtoReflect.Descriptor instead.
+func (*WriteStream) Descriptor() ([]byte, []int) {
+	return file_types_transfer_streaming_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *WriteStream) GetStream() string {
+	if x != nil {
+		return x.Stream
+	}
+	return ""
+}
+
+func (x *WriteStream) GetMediaType() string {
+	if x != nil {
+		return x.MediaType
+	}
+	return ""
+}
+
 var File_types_transfer_streaming_proto protoreflect.FileDescriptor
 
 var file_types_transfer_streaming_proto_rawDesc = []byte{
@@ -139,11 +255,20 @@ var file_types_transfer_streaming_proto_rawDesc = []byte{
 	0x61, 0x74, 0x61, 0x12, 0x12, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x01, 0x20, 0x01, 0x28,
 	0x0c, 0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x22, 0x26, 0x0a, 0x0c, 0x57, 0x69, 0x6e, 0x64, 0x6f,
 	0x77, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x12, 0x16, 0x0a, 0x06, 0x75, 0x70, 0x64, 0x61, 0x74,
-	0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x06, 0x75, 0x70, 0x64, 0x61, 0x74, 0x65, 0x42,
-	0x35, 0x5a, 0x33, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x6f,
-	0x6e, 0x74, 0x61, 0x69, 0x6e, 0x65, 0x72, 0x64, 0x2f, 0x63, 0x6f, 0x6e, 0x74, 0x61, 0x69, 0x6e,
-	0x65, 0x72, 0x64, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x74, 0x79, 0x70, 0x65, 0x73, 0x2f, 0x74, 0x72,
-	0x61, 0x6e, 0x73, 0x66, 0x65, 0x72, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x06, 0x75, 0x70, 0x64, 0x61, 0x74, 0x65, 0x22,
+	0x43, 0x0a, 0x0a, 0x52, 0x65, 0x61, 0x64, 0x53, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x12, 0x16, 0x0a,
+	0x06, 0x73, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x73,
+	0x74, 0x72, 0x65, 0x61, 0x6d, 0x12, 0x1d, 0x0a, 0x0a, 0x6d, 0x65, 0x64, 0x69, 0x61, 0x5f, 0x74,
+	0x79, 0x70, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x09, 0x6d, 0x65, 0x64, 0x69, 0x61,
+	0x54, 0x79, 0x70, 0x65, 0x22, 0x44, 0x0a, 0x0b, 0x57, 0x72, 0x69, 0x74, 0x65, 0x53, 0x74, 0x72,
+	0x65, 0x61, 0x6d, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x18, 0x01, 0x20,
+	0x01, 0x28, 0x09, 0x52, 0x06, 0x73, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x12, 0x1d, 0x0a, 0x0a, 0x6d,
+	0x65, 0x64, 0x69, 0x61, 0x5f, 0x74, 0x79, 0x70, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x09, 0x6d, 0x65, 0x64, 0x69, 0x61, 0x54, 0x79, 0x70, 0x65, 0x42, 0x35, 0x5a, 0x33, 0x67, 0x69,
+	0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x6f, 0x6e, 0x74, 0x61, 0x69, 0x6e,
+	0x65, 0x72, 0x64, 0x2f, 0x63, 0x6f, 0x6e, 0x74, 0x61, 0x69, 0x6e, 0x65, 0x72, 0x64, 0x2f, 0x61,
+	0x70, 0x69, 0x2f, 0x74, 0x79, 0x70, 0x65, 0x73, 0x2f, 0x74, 0x72, 0x61, 0x6e, 0x73, 0x66, 0x65,
+	0x72, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -158,10 +283,12 @@ func file_types_transfer_streaming_proto_rawDescGZIP() []byte {
 	return file_types_transfer_streaming_proto_rawDescData
 }
 
-var file_types_transfer_streaming_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_types_transfer_streaming_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_types_transfer_streaming_proto_goTypes = []interface{}{
 	(*Data)(nil),         // 0: containerd.types.transfer.Data
 	(*WindowUpdate)(nil), // 1: containerd.types.transfer.WindowUpdate
+	(*ReadStream)(nil),   // 2: containerd.types.transfer.ReadStream
+	(*WriteStream)(nil),  // 3: containerd.types.transfer.WriteStream
 }
 var file_types_transfer_streaming_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -201,6 +328,30 @@ func file_types_transfer_streaming_proto_init() {
 				return nil
 			}
 		}
+		file_types_transfer_streaming_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ReadStream); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_types_transfer_streaming_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*WriteStream); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -208,7 +359,7 @@ func file_types_transfer_streaming_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_types_transfer_streaming_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
