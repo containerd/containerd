3d56a26d55f9f81ef6acb3f1c4b50c7c2b5e0cb2
Mike Ma <mike.ma@lmctl.com>
2026-05-07 11:45:31 -0500

    Handle SIGHUP without stopping containerd
    containerd does not currently support runtime config reload, but receiving
    SIGHUP should not terminate the daemon. On Unix, SIGHUP was not registered in
    `handledSignals`, so default OS behavior could terminate containerd and trigger
    follow-on shim log errors during shutdown paths.
    
    Register SIGHUP in the signal handler and treat it as a no-op with a warning.
    This prevents daemon exit while keeping existing SIGTERM/SIGINT shutdown
    behavior unchanged.
    
    Add a focused unit test to verify SIGHUP is ignored and SIGTERM still stops
    the signal loop.
    
    Tested: docker run --rm -v "$PWD":/src -w /src golang:1.26 go test ./cmd/containerd/command -run TestHandleSignalsIgnoreSIGHUP -count=1

---
+++ cmd/containerd/command/main_unix_test.go
@@ -0,0 +1,65 @@
+//go:build linux || darwin || freebsd || solaris
+
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package command
+
+import (
+	"context"
+	"os"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/containerd/containerd/v2/cmd/containerd/server"
+	"golang.org/x/sys/unix"
+)
+
+func TestHandleSignalsIgnoreSIGHUP(t *testing.T) {
+	signals := make(chan os.Signal, 2)
+	serverC := make(chan *server.Server, 1)
+	var cancelCalled atomic.Bool
+	cancel := func() {
+		cancelCalled.Store(true)
+	}
+
+	done := handleSignals(context.Background(), signals, serverC, cancel)
+
+	signals <- unix.SIGHUP
+
+	select {
+	case <-done:
+		t.Fatal("signal handler exited on SIGHUP")
+	case <-time.After(100 * time.Millisecond):
+	}
+
+	if cancelCalled.Load() {
+		t.Fatal("cancel should not be called for SIGHUP")
+	}
+
+	signals <- unix.SIGTERM
+
+	select {
+	case <-done:
+	case <-time.After(2 * time.Second):
+		t.Fatal("signal handler did not exit on SIGTERM")
+	}
+
+	if !cancelCalled.Load() {
+		t.Fatal("cancel should be called for SIGTERM")
+	}
+}
