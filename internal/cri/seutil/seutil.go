a005e0fec876a4cb7aecefdba0a037b44282d714
Jin Dong <djdongjin95@gmail.com>
2026-06-13 19:15:10 +0000

    cri: update selinux API usage
    Signed-off-by: Jin Dong <djdongjin95@gmail.com>

--- internal/cri/seutil/seutil.go
+++ internal/cri/seutil/seutil.go
@@ -25,17 +25,5 @@ func ChangeToKVM(l string) (string, error) {
 	if l == "" || !selinux.GetEnabled() {
 		return "", nil
 	}
-	proc, _ := selinux.KVMContainerLabels()
-	selinux.ReleaseLabel(proc)
-
-	current, err := selinux.NewContext(l)
-	if err != nil {
-		return "", err
-	}
-	next, err := selinux.NewContext(proc)
-	if err != nil {
-		return "", err
-	}
-	current["type"] = next["type"]
-	return current.Get(), nil
+	return selinux.SetProcessKind(l, selinux.ProcessKindKVM)
 }
